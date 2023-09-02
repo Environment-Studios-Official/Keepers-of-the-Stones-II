d9be5dedec125b1d1a9600af63315176079d12d4
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-09-02 12:30:55 +0300

    Update logo

--- src/main/java/ru/power_umc/keepersofthestones/two/item/AirStoneItem.java
+++ src/main/java/ru/power_umc/keepersofthestones/two/item/AirStoneItem.java
@@ -18,7 +18,7 @@ public class AirStoneItem extends Item {
 
 	@Override
 	public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
-		return 0F;
+		return 0f;
 	}
 
 	@Override
--- src/main/java/ru/power_umc/keepersofthestones/two/item/FireStoneItem.java
+++ src/main/java/ru/power_umc/keepersofthestones/two/item/FireStoneItem.java
@@ -23,7 +23,7 @@ public class FireStoneItem extends Item {
 
 	@Override
 	public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
-		return 0F;
+		return 0f;
 	}
 
 	@Override
