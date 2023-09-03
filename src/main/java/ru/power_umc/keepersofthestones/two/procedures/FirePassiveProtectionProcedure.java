9efb7fffa7cbe8365925bbdb829adc961dde5790
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-09-03 22:58:18 +0300

    Added passive protection in lava

---
+++ src/main/java/ru/power_umc/keepersofthestones/two/procedures/FirePassiveProtectionProcedure.java
@@ -0,0 +1,54 @@
+package ru.power_umc.keepersofthestones.two.procedures;
+
+import ru.power_umc.keepersofthestones.two.init.PowerModMobEffects;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.core.particles.ParticleTypes;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class FirePassiveProtectionProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player.level(), event.player.getX(), event.player.getY(), event.player.getZ(), event.player);
+		}
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		double particleRadius = 0;
+		double particleAmount = 0;
+		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(PowerModMobEffects.FIRE_MASTER.get())) {
+			if (entity.isInLava() == true) {
+				particleAmount = 8;
+				particleRadius = 2;
+				for (int index0 = 0; index0 < (int) particleAmount; index0++) {
+					world.addParticle(ParticleTypes.FLAME, (x + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (y + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius),
+							(z + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)),
+							(Mth.nextDouble(RandomSource.create(), -0.001, 0.001)));
+				}
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 400, 0, false, false));
+				entity.clearFire();
+			}
+		}
+	}
+}
