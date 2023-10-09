7a4e2187652edf89030a58a0e40688d07fbbf171
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-09 22:47:21 +0300

    Fix color for Lightning

--- src/main/java/ru/power_umc/keepersofthestones/two/procedures/LightningMasterStartProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestones/two/procedures/LightningMasterStartProcedure.java
@@ -40,7 +40,7 @@ public class LightningMasterStartProcedure {
 			Entity _ent = entity;
 			if (!_ent.level().isClientSide() && _ent.getServer() != null) {
 				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-						_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "team modify lightning color red");
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "team modify lightning color light_purple");
 			}
 		}
 		{
