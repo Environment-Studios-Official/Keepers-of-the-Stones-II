b7a7806f6999975b22e4b24efeebdb39ed86f973
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-18 18:34:46 +0300

    Removed unused sound

--- src/main/java/ru/power_umc/keepersofthestonestwo/init/PowerModSounds.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/init/PowerModSounds.java
@@ -18,6 +18,5 @@ public class PowerModSounds {
 	public static final RegistryObject<SoundEvent> STONE_ACTIVATION = REGISTRY.register("stone_activation", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("power", "stone_activation")));
 	public static final RegistryObject<SoundEvent> STONE_DEACTIVATION = REGISTRY.register("stone_deactivation", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("power", "stone_deactivation")));
 	public static final RegistryObject<SoundEvent> ELECTRIC_SPARK = REGISTRY.register("electric_spark", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("power", "electric_spark")));
-	public static final RegistryObject<SoundEvent> BASS_BOOM = REGISTRY.register("bass_boom", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("power", "bass_boom")));
 	public static final RegistryObject<SoundEvent> EAR_RINGING = REGISTRY.register("ear_ringing", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("power", "ear_ringing")));
 }
