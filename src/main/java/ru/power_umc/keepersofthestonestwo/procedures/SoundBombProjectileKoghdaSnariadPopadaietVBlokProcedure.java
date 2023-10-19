11ec5cf4b4c3fb0e110685248ca9df7b9040f277
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-19 18:05:33 +0300

    Little improvements

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/SoundBombProjectileKoghdaSnariadPopadaietVBlokProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/SoundBombProjectileKoghdaSnariadPopadaietVBlokProcedure.java
@@ -15,10 +15,10 @@ import java.util.Comparator;
 public class SoundBombProjectileKoghdaSnariadPopadaietVBlokProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		if (world instanceof Level _level && !_level.isClientSide())
-			_level.explode(null, x, y, z, 2, Level.ExplosionInteraction.NONE);
+			_level.explode(null, x, y, z, 3, Level.ExplosionInteraction.NONE);
 		{
 			final Vec3 _center = new Vec3(x, y, z);
-			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(2 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(3 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 			for (Entity entityiterator : _entfound) {
 				{
 					Entity _ent = entityiterator;
