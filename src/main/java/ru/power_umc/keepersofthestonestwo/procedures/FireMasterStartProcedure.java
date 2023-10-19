f879f019588921d8aae742f058308be7cb318d90
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-19 10:13:41 +0300

    Refactor master effects (Part 1)

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/FireMasterStartProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/FireMasterStartProcedure.java
@@ -1,11 +1,16 @@
 package ru.power_umc.keepersofthestonestwo.procedures;
 
 import ru.power_umc.keepersofthestonestwo.network.PowerModVariables;
+import ru.power_umc.keepersofthestonestwo.init.PowerModItems;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -61,34 +66,45 @@ public class FireMasterStartProcedure {
 		}
 		if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).mergers == 0) {
 			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "item replace entity @s armor.head with power:fire_armor_helmet{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(3, new ItemStack(PowerModItems.FIRE_ARMOR_HELMET.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.HEAD, new ItemStack(PowerModItems.FIRE_ARMOR_HELMET.get()));
 				}
 			}
 			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent),
-							"item replace entity @s armor.chest with power:fire_armor_chestplate{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(2, new ItemStack(PowerModItems.FIRE_ARMOR_CHESTPLATE.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.CHEST, new ItemStack(PowerModItems.FIRE_ARMOR_CHESTPLATE.get()));
 				}
 			}
 			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "item replace entity @s armor.legs with power:fire_armor_leggings{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(1, new ItemStack(PowerModItems.FIRE_ARMOR_LEGGINGS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(PowerModItems.FIRE_ARMOR_LEGGINGS.get()));
 				}
 			}
 			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "item replace entity @s armor.feet with power:fire_armor_boots{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(0, new ItemStack(PowerModItems.FIRE_ARMOR_BOOTS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(PowerModItems.FIRE_ARMOR_BOOTS.get()));
 				}
 			}
+			(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.HEAD) : ItemStack.EMPTY).enchant(Enchantments.BINDING_CURSE, 1);
+			(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).enchant(Enchantments.BINDING_CURSE, 1);
+			(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY).enchant(Enchantments.BINDING_CURSE, 1);
+			(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).enchant(Enchantments.BINDING_CURSE, 1);
 			{
 				double _setval = 1;
 				entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
@@ -105,19 +121,25 @@ public class FireMasterStartProcedure {
 			}
 		} else if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).mergers == 1) {
 			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "item replace entity @s armor.legs with power:fire_armor_leggings{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(1, new ItemStack(PowerModItems.FIRE_ARMOR_LEGGINGS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(PowerModItems.FIRE_ARMOR_LEGGINGS.get()));
 				}
 			}
 			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "item replace entity @s armor.feet with power:fire_armor_boots{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(0, new ItemStack(PowerModItems.FIRE_ARMOR_BOOTS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(PowerModItems.FIRE_ARMOR_BOOTS.get()));
 				}
 			}
+			(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY).enchant(Enchantments.BINDING_CURSE, 1);
+			(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).enchant(Enchantments.BINDING_CURSE, 1);
 			{
 				double _setval = 1;
 				entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
@@ -134,12 +156,15 @@ public class FireMasterStartProcedure {
 			}
 		} else if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).mergers == 2) {
 			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "item replace entity @s armor.legs with power:fire_armor_leggings{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(1, new ItemStack(PowerModItems.FIRE_ARMOR_LEGGINGS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(PowerModItems.FIRE_ARMOR_LEGGINGS.get()));
 				}
 			}
+			(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY).enchant(Enchantments.BINDING_CURSE, 1);
 			{
 				double _setval = 1;
 				entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
