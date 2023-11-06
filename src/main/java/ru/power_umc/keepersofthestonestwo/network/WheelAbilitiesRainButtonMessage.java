099d5f3bbca4b3fb150f47e08c38c1897b3ea081
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-11-06 22:35:23 +0300

    Finished rain element

--- src/main/java/ru/power_umc/keepersofthestonestwo/network/WheelAbilitiesRainButtonMessage.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/network/WheelAbilitiesRainButtonMessage.java
@@ -69,27 +69,27 @@ public class WheelAbilitiesRainButtonMessage {
 			return;
 		if (buttonID == 0) {
 
-			Attack41Procedure.execute(entity);
+			OpenWheelOneProcedure.execute(world, x, y, z, entity);
 		}
 		if (buttonID == 1) {
 
-			Attack42Procedure.execute(entity);
+			OpenWheelTwoProcedure.execute(world, x, y, z, entity);
 		}
 		if (buttonID == 2) {
 
-			Attack43Procedure.execute(entity);
+			OpenWheelThreeProcedure.execute(world, x, y, z, entity);
 		}
 		if (buttonID == 3) {
 
-			OpenWheelOneProcedure.execute(world, x, y, z, entity);
+			Attack41Procedure.execute(entity);
 		}
 		if (buttonID == 4) {
 
-			OpenWheelTwoProcedure.execute(world, x, y, z, entity);
+			Attack42Procedure.execute(entity);
 		}
 		if (buttonID == 5) {
 
-			OpenWheelThreeProcedure.execute(world, x, y, z, entity);
+			Attack43Procedure.execute(entity);
 		}
 	}
 
