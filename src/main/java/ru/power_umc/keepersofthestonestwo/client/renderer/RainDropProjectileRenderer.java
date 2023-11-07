a8b8a215d429eb1f7e787693728779bc41c085a2
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-11-07 23:32:55 +0300

    Fix renderer for rain drop

--- src/main/java/ru/power_umc/keepersofthestonestwo/client/renderer/RainDropProjectileRenderer.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/client/renderer/RainDropProjectileRenderer.java
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.MultiBufferSource;
 
+import com.mojang.math.Axis;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.PoseStack;
 
