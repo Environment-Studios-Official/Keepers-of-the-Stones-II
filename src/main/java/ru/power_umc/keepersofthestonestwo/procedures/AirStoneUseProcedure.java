217be41f1c458c8536000f62ba5548793550b08f
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-11-14 15:41:49 +0300

    Some fixes

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/AirStoneUseProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/AirStoneUseProcedure.java
@@ -17,6 +17,13 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
+import net.minecraft.client.player.AbstractClientPlayer;
+
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationAccess;
+import dev.kosmx.playerAnim.api.layered.ModifierLayer;
+import dev.kosmx.playerAnim.api.layered.KeyframeAnimationPlayer;
+import dev.kosmx.playerAnim.api.layered.IAnimation;
 
 public class AirStoneUseProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
@@ -50,6 +57,14 @@ public class AirStoneUseProcedure {
 						capability.power = _setval;
 						capability.syncPlayerVariables(entity);
 					});
+				}
+				if (world.isClientSide()) {
+					if (entity instanceof AbstractClientPlayer player) {
+						var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData(player).get(new ResourceLocation("power", "player_animation"));
+						if (animation != null && !animation.isActive()) {
+							animation.setAnimation(new KeyframeAnimationPlayer(PlayerAnimationRegistry.getAnimation(new ResourceLocation("power", "animation.player.transformation"))));
+						}
+					}
 				}
 				if (world instanceof Level _level) {
 					if (!_level.isClientSide()) {
