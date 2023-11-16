f55cece55afe11aebfcb726d6e4045f15e04da59
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-11-16 23:05:40 +0300

    Added third ocean element ability

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/SpecialAttackProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/SpecialAttackProcedure.java
@@ -2505,6 +2505,29 @@ public class SpecialAttackProcedure {
 							});
 						}
 					}
+				} else if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).attack == 51) {
+					if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).power >= 5) {
+						if (world instanceof Level _level) {
+							if (!_level.isClientSide()) {
+								_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.splash")), SoundSource.PLAYERS, 1, 1);
+							} else {
+								_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.splash")), SoundSource.PLAYERS, 1, 1, false);
+							}
+						}
+						if (world instanceof ServerLevel _level)
+							_level.sendParticles(ParticleTypes.BUBBLE_POP, x, y, z, 30, 0, (-1), 0, 1);
+						if (world instanceof ServerLevel _level)
+							_level.sendParticles(ParticleTypes.BUBBLE_POP, x, y, z, 30, 0, 1, 0, 1);
+						if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+							_entity.addEffect(new MobEffectInstance(MobEffects.DOLPHINS_GRACE, 100, 9, false, false));
+						{
+							double _setval = (entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).power - 5;
+							entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+								capability.power = _setval;
+								capability.syncPlayerVariables(entity);
+							});
+						}
+					}
 				}
 			}
 		}
