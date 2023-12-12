de25e623e50d13026b2b6f2874b1f15450a65aa1
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-12-12 18:38:48 +0300

    Change color for earth battery description

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/EarthBatteryDescProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/EarthBatteryDescProcedure.java
@@ -4,6 +4,6 @@ import net.minecraft.network.chat.Component;
 
 public class EarthBatteryDescProcedure {
 	public static String execute() {
-		return "\u00A70\u00A7o" + Component.translatable("battery.desc.earth").getString();
+		return "\u00A78\u00A7o" + Component.translatable("battery.desc.earth").getString();
 	}
 }
