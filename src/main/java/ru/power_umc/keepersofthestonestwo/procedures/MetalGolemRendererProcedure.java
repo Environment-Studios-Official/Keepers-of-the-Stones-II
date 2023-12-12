7714da5640dfef0cb92fb5a73793d48e63bec4a7
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-12-12 17:12:56 +0300

    Some fixes

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/MetalGolemRendererProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/MetalGolemRendererProcedure.java
@@ -1,35 +1,6 @@
 package ru.power_umc.keepersofthestonestwo.procedures;
 
-import ru.power_umc.keepersofthestonestwo.init.PowerModMobEffects;
-
-import net.minecraftforge.client.event.RenderLivingEvent;
-
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.Minecraft;
-
 public class MetalGolemRendererProcedure {
-	public static void execute(Entity entity) {
-		if (entity == null)
-			return;
-		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(PowerModMobEffects.IRON_SKIN.get())) {
-			RenderLivingEvent _evt = (RenderLivingEvent) event;
-			Minecraft mc = Minecraft.getInstance();
-			EntityRenderDispatcher dis = Minecraft.getInstance().getEntityRenderDispatcher();
-			EntityRendererProvider.Context context = new EntityRendererProvider.Context(dis, mc.getItemRenderer(), mc.getBlockRenderer(), dis.getItemInHandRenderer(), mc.getResourceManager(), mc.getEntityModels(), mc.font);
-			if (_evt.getRenderer() instanceof PlayerRenderer) {
-				if (_evt instanceof RenderLivingEvent.Pre) {
-					_evt.setCanceled(true);
-				}
-				new com.kleiders.kleidersplayerrenderer.ClassicPlayerRenderer(new EntityRendererProvider.Context(dis, mc.getItemRenderer(), mc.getBlockRenderer(), dis.getItemInHandRenderer(), mc.getResourceManager(), mc.getEntityModels(), mc.font),
-						new ResourceLocation("power:textures/entities/metal_form.png"))
-						.render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
-			}
-		}
+	public static void execute() {
 	}
 }
