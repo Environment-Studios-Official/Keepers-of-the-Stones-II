14cbe75dae6f05dfd2678156887742e507dc19a1
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-12-21 23:02:26 +0300

    Some fixes

--- src/main/java/ru/power_umc/keepersofthestonestwo/init/PowerModTabs.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/init/PowerModTabs.java
@@ -36,6 +36,7 @@ public class PowerModTabs {
 				tabData.accept(PowerModItems.METAL_STONE.get());
 				tabData.accept(PowerModItems.LIGHT_STONE.get());
 				tabData.accept(PowerModItems.SHADOW_STONE.get());
+				tabData.accept(PowerModItems.VACUUM_STONE.get());
 			})
 
 					.build());
