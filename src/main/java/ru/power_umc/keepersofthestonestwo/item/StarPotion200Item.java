26068668b3b98658081238d5f3719eb129af3dba
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-01-13 18:22:02 +0300

    Star potion now has a glow

--- src/main/java/ru/power_umc/keepersofthestonestwo/item/StarPotion200Item.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/item/StarPotion200Item.java
@@ -4,6 +4,9 @@ package ru.power_umc.keepersofthestonestwo.item;
 import ru.power_umc.keepersofthestonestwo.procedures.StarPotion200PriShchielchkiePKMProcedure;
 import ru.power_umc.keepersofthestonestwo.procedures.StarPotion200DopolnitielnaiaInformatsiiaProcedure;
 
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
@@ -33,6 +36,12 @@ public class StarPotion200Item extends Item {
 	@Override
 	public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 		return 0f;
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public boolean isFoil(ItemStack itemstack) {
+		return true;
 	}
 
 	@Override
