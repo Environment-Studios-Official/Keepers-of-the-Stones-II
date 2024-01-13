32f40ff85c5e53010e537408526e2e291765d50a
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-01-13 19:48:16 +0300

    Fixed a bug with mixed textures of energium armor

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -2,4 +2,4 @@ What's new in Keepers of the Stones version II 1.0.0.8?<br />
 **#WARNING! Some features may not be displayed correctly on clients due to poor synchronization.**
 <br />- Star potion now has a glow
 <br />- Improved generation of energium ore
-<br />- [Bugfix] 
+<br />- [Bugfix] Fixed a bug with mixed textures of energium armor
---
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/SphereNothingProjectileRadiusAttackProcedure.java
@@ -0,0 +1,38 @@
+package ru.power_umc.keepersofthestonestwo.procedures;
+
+import ru.power_umc.keepersofthestonestwo.init.PowerModParticleTypes;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.core.particles.SimpleParticleType;
+
+import java.util.List;
+import java.util.Comparator;
+
+public class SphereNothingProjectileRadiusAttackProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z) {
+		double particleRadius = 0;
+		double particleAmount = 0;
+		particleAmount = 16;
+		particleRadius = 3;
+		for (int index0 = 0; index0 < (int) particleAmount; index0++) {
+			world.addParticle((SimpleParticleType) (PowerModParticleTypes.VACUUM_PARTICLE.get()), (x + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (y + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius),
+					(z + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)),
+					(Mth.nextDouble(RandomSource.create(), -0.001, 0.001)));
+		}
+		{
+			final Vec3 _center = new Vec3(x, y, z);
+			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(3 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+			for (Entity entityiterator : _entfound) {
+				entityiterator.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.MAGIC)), 9);
+			}
+		}
+	}
+}
