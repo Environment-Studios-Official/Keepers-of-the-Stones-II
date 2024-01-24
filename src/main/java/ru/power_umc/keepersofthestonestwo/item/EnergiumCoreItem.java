c00b0acaa200d5890bf5529bf2d3439a26d9c3a5
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-01-24 21:20:24 +0300

    Some fixes

--- src/main/java/ru/power_umc/keepersofthestonestwo/item/EnergiumCoreItem.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/item/EnergiumCoreItem.java
@@ -1,12 +1,15 @@
 
 package ru.power_umc.keepersofthestonestwo.item;
 
+import ru.power_umc.keepersofthestonestwo.procedures.EnergiumItemsPowerLockedProcedure;
+
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.Component;
 
 import java.util.List;
@@ -24,5 +27,11 @@ public class EnergiumCoreItem extends Item {
 	@Override
 	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, world, list, flag);
+	}
+
+	@Override
+	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(itemstack, world, entity, slot, selected);
+		EnergiumItemsPowerLockedProcedure.execute(entity, itemstack);
 	}
 }
