f3141a1a0a19c173b4761cf327b855fedcc93b18
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-01-30 16:35:58 +0300

    Some improvements

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/GetRechargeInfoProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/GetRechargeInfoProcedure.java
@@ -5,6 +5,9 @@ import net.minecraft.network.chat.Component;
 
 public class GetRechargeInfoProcedure {
 	public static String execute(ItemStack itemstack) {
-		return "\u00A77" + Component.translatable("power.modinfo.recharge_stone").getString() + Math.round(itemstack.getOrCreateTag().getDouble("rechargeStone") / 20) + Component.translatable("power.modinfo.recharge_stone.seconds").getString();
+		return itemstack.getOrCreateTag().getDouble("rechargeStone") > 0
+				? "\u00A77\u00A7o" + Component.translatable("power.modinfo.recharge_stone").getString() + Math.round(itemstack.getOrCreateTag().getDouble("rechargeStone") / 20)
+						+ Component.translatable("power.modinfo.recharge_stone.seconds").getString()
+				: "\u00A7a\u00A7o" + Component.translatable("power.modinfo.recharge_stone.done").getString();
 	}
 }
