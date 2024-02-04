4b9415756b0eac202ddd408f691a5d3ebe00e659
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-02-04 23:42:37 +0300

    Some optimizations

--- src/main/java/ru/power_umc/keepersofthestonestwo/item/SunStoneItem.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/item/SunStoneItem.java
@@ -49,6 +49,6 @@ public class SunStoneItem extends Item {
 	@Override
 	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
 		super.inventoryTick(itemstack, world, entity, slot, selected);
-		RechargeStoneTickEventProcedure.execute(world, itemstack);
+		RechargeStoneTickEventProcedure.execute(itemstack);
 	}
 }
