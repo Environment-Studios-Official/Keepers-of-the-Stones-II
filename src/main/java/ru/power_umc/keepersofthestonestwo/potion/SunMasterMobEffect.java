f4a654a5f13e3114b51c09fa3dc51d354f5b9242
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-02-04 23:11:04 +0300

    Fix sun master effect

--- src/main/java/ru/power_umc/keepersofthestonestwo/potion/SunMasterMobEffect.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/potion/SunMasterMobEffect.java
@@ -1,6 +1,12 @@
 
 package ru.power_umc.keepersofthestonestwo.potion;
 
+import ru.power_umc.keepersofthestonestwo.procedures.SunMasterStartProcedure;
+import ru.power_umc.keepersofthestonestwo.procedures.SunMasterEndProcedure;
+import ru.power_umc.keepersofthestonestwo.procedures.EnhancedAbilitiesProcedure;
+
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffect;
 
@@ -12,6 +18,22 @@ public class SunMasterMobEffect extends MobEffect {
 	@Override
 	public String getDescriptionId() {
 		return "effect.power.sun_master";
+	}
+
+	@Override
+	public void addAttributeModifiers(LivingEntity entity, AttributeMap attributeMap, int amplifier) {
+		SunMasterStartProcedure.execute(entity.level(), entity.getX(), entity.getY(), entity.getZ(), entity);
+	}
+
+	@Override
+	public void applyEffectTick(LivingEntity entity, int amplifier) {
+		EnhancedAbilitiesProcedure.execute(entity);
+	}
+
+	@Override
+	public void removeAttributeModifiers(LivingEntity entity, AttributeMap attributeMap, int amplifier) {
+		super.removeAttributeModifiers(entity, attributeMap, amplifier);
+		SunMasterEndProcedure.execute(entity.level(), entity.getX(), entity.getY(), entity.getZ(), entity);
 	}
 
 	@Override
