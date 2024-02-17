5e5b8f1b285427efb09683697ab86562be5a444d
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-02-17 23:21:08 +0300

    Sun is complete

--- src/main/java/ru/power_umc/keepersofthestonestwo/client/gui/WheelAbilitiesSunScreen.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/client/gui/WheelAbilitiesSunScreen.java
@@ -13,7 +13,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.components.ImageButton;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.GuiGraphics;
 
 import java.util.HashMap;
@@ -25,12 +24,12 @@ public class WheelAbilitiesSunScreen extends AbstractContainerScreen<WheelAbilit
 	private final Level world;
 	private final int x, y, z;
 	private final Player entity;
-	Button button_1;
-	Button button_2;
-	Button button_3;
 	ImageButton imagebutton_wheel_button_1;
 	ImageButton imagebutton_wheel_button_2;
 	ImageButton imagebutton_wheel_button_3;
+	ImageButton imagebutton_sun_ray;
+	ImageButton imagebutton_sun_explosion;
+	ImageButton imagebutton_sun_healing;
 
 	public WheelAbilitiesSunScreen(WheelAbilitiesSunMenu container, Inventory inventory, Component text) {
 		super(container, inventory, text);
@@ -48,6 +47,12 @@ public class WheelAbilitiesSunScreen extends AbstractContainerScreen<WheelAbilit
 		this.renderBackground(guiGraphics);
 		super.render(guiGraphics, mouseX, mouseY, partialTicks);
 		this.renderTooltip(guiGraphics, mouseX, mouseY);
+		if (mouseX > leftPos + 82 && mouseX < leftPos + 106 && mouseY > topPos + 22 && mouseY < topPos + 46)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_sun.tooltip_sun_ray_uses_15"), mouseX, mouseY);
+		if (mouseX > leftPos + 144 && mouseX < leftPos + 168 && mouseY > topPos + 82 && mouseY < topPos + 106)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_sun.tooltip_sun_explosion_uses_45"), mouseX, mouseY);
+		if (mouseX > leftPos + 82 && mouseX < leftPos + 106 && mouseY > topPos + 146 && mouseY < topPos + 170)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_sun.tooltip_sun_healing_uses_80"), mouseX, mouseY);
 	}
 
 	@Override
@@ -87,34 +92,10 @@ public class WheelAbilitiesSunScreen extends AbstractContainerScreen<WheelAbilit
 	@Override
 	public void init() {
 		super.init();
-		button_1 = Button.builder(Component.translatable("gui.power.wheel_abilities_sun.button_1"), e -> {
-			if (true) {
-				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(0, x, y, z));
-				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 0, x, y, z);
-			}
-		}).bounds(this.leftPos + 80, this.topPos + 26, 30, 20).build();
-		guistate.put("button:button_1", button_1);
-		this.addRenderableWidget(button_1);
-		button_2 = Button.builder(Component.translatable("gui.power.wheel_abilities_sun.button_2"), e -> {
-			if (true) {
-				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(1, x, y, z));
-				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 1, x, y, z);
-			}
-		}).bounds(this.leftPos + 141, this.topPos + 86, 30, 20).build();
-		guistate.put("button:button_2", button_2);
-		this.addRenderableWidget(button_2);
-		button_3 = Button.builder(Component.translatable("gui.power.wheel_abilities_sun.button_3"), e -> {
-			if (true) {
-				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(2, x, y, z));
-				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 2, x, y, z);
-			}
-		}).bounds(this.leftPos + 79, this.topPos + 147, 30, 20).build();
-		guistate.put("button:button_3", button_3);
-		this.addRenderableWidget(button_3);
 		imagebutton_wheel_button_1 = new ImageButton(this.leftPos + 140, this.topPos + 154, 10, 7, 0, 0, 7, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_1.png"), 10, 14, e -> {
 			if (GetWheelTwoProcedure.execute(entity)) {
-				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(3, x, y, z));
-				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 3, x, y, z);
+				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(0, x, y, z));
+				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 0, x, y, z);
 			}
 		}) {
 			@Override
@@ -127,8 +108,8 @@ public class WheelAbilitiesSunScreen extends AbstractContainerScreen<WheelAbilit
 		this.addRenderableWidget(imagebutton_wheel_button_1);
 		imagebutton_wheel_button_2 = new ImageButton(this.leftPos + 152, this.topPos + 154, 10, 7, 0, 0, 7, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_2.png"), 10, 14, e -> {
 			if (GetWheelTwoProcedure.execute(entity)) {
-				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(4, x, y, z));
-				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 4, x, y, z);
+				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(1, x, y, z));
+				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 1, x, y, z);
 			}
 		}) {
 			@Override
@@ -141,8 +122,8 @@ public class WheelAbilitiesSunScreen extends AbstractContainerScreen<WheelAbilit
 		this.addRenderableWidget(imagebutton_wheel_button_2);
 		imagebutton_wheel_button_3 = new ImageButton(this.leftPos + 164, this.topPos + 154, 10, 7, 0, 0, 7, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_3.png"), 10, 14, e -> {
 			if (GetWheelThreeProcedure.execute(entity)) {
-				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(5, x, y, z));
-				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 5, x, y, z);
+				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(2, x, y, z));
+				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 2, x, y, z);
 			}
 		}) {
 			@Override
@@ -153,5 +134,29 @@ public class WheelAbilitiesSunScreen extends AbstractContainerScreen<WheelAbilit
 		};
 		guistate.put("button:imagebutton_wheel_button_3", imagebutton_wheel_button_3);
 		this.addRenderableWidget(imagebutton_wheel_button_3);
+		imagebutton_sun_ray = new ImageButton(this.leftPos + 72, this.topPos + 12, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_sun_ray.png"), 46, 92, e -> {
+			if (true) {
+				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(3, x, y, z));
+				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 3, x, y, z);
+			}
+		});
+		guistate.put("button:imagebutton_sun_ray", imagebutton_sun_ray);
+		this.addRenderableWidget(imagebutton_sun_ray);
+		imagebutton_sun_explosion = new ImageButton(this.leftPos + 133, this.topPos + 73, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_sun_explosion.png"), 46, 92, e -> {
+			if (true) {
+				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(4, x, y, z));
+				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 4, x, y, z);
+			}
+		});
+		guistate.put("button:imagebutton_sun_explosion", imagebutton_sun_explosion);
+		this.addRenderableWidget(imagebutton_sun_explosion);
+		imagebutton_sun_healing = new ImageButton(this.leftPos + 72, this.topPos + 134, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_sun_healing.png"), 46, 92, e -> {
+			if (true) {
+				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesSunButtonMessage(5, x, y, z));
+				WheelAbilitiesSunButtonMessage.handleButtonAction(entity, 5, x, y, z);
+			}
+		});
+		guistate.put("button:imagebutton_sun_healing", imagebutton_sun_healing);
+		this.addRenderableWidget(imagebutton_sun_healing);
 	}
 }
