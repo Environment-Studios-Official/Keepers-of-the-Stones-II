e5ebeb39d0fdd036ac42a70d3c40b249f24a6b4a
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-02-20 01:17:24 +0300

    Some fixes

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/OceanMasterEndProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/OceanMasterEndProcedure.java
@@ -26,15 +26,7 @@ public class OceanMasterEndProcedure {
 				_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("power:stone_deactivation")), SoundSource.PLAYERS, 1, 1, false);
 			}
 		}
-		if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).mergers == 0) {
-			{
-				boolean _setval = false;
-				entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-					capability.active = _setval;
-					capability.syncPlayerVariables(entity);
-				});
-			}
-		}
+		DetransformationKeyUseProcedure.execute(world, entity);
 		{
 			double _setval = (entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).mergers - 1;
 			entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/TornadoMasterEndProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/TornadoMasterEndProcedure.java
@@ -26,22 +26,7 @@ public class TornadoMasterEndProcedure {
 				_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("power:stone_deactivation")), SoundSource.PLAYERS, 1, 1, false);
 			}
 		}
-		if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).mergers == 0) {
-			{
-				boolean _setval = false;
-				entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-					capability.active = _setval;
-					capability.syncPlayerVariables(entity);
-				});
-			}
-		}
-		{
-			double _setval = (entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).mergers - 1;
-			entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-				capability.mergers = _setval;
-				capability.syncPlayerVariables(entity);
-			});
-		}
+		DetransformationKeyUseProcedure.execute(world, entity);
 		if (((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).element_name_first).equals("tornado")) {
 			{
 				String _setval = "0";
