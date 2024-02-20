aab5674fd94d29d43982687e54159a988c285cca
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-02-20 14:33:14 +0300

    Fixed color for moon battery

--- src/main/java/ru/power_umc/keepersofthestonestwo/procedures/MoonBatteryDescProcedure.java
+++ src/main/java/ru/power_umc/keepersofthestonestwo/procedures/MoonBatteryDescProcedure.java
@@ -4,6 +4,6 @@ import net.minecraft.network.chat.Component;
 
 public class MoonBatteryDescProcedure {
 	public static String execute() {
-		return "\u00A7e\u00A7o" + Component.translatable("battery.desc.moon").getString();
+		return "\u00A7b\u00A7o" + Component.translatable("battery.desc.moon").getString();
 	}
 }
