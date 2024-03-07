b7bf2a8e174ded8e8ee4e92169736defad595660
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-03-07 23:25:16 +0300

    Added passive ability for space

---
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/SpacePassiveAbIlityProcedure.java
@@ -0,0 +1,46 @@
+package com.esmods.keepersofthestonestwo.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+
+import javax.annotation.Nullable;
+
+import com.esmods.keepersofthestonestwo.init.PowerModMobEffects;
+
+@Mod.EventBusSubscriber
+public class SpacePassiveAbIlityProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player);
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		double particleRadius = 0;
+		double particleAmount = 0;
+		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(PowerModMobEffects.SPACE_MASTER.get())) {
+			if (entity.isInLava() || entity.isOnFire()) {
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 400, 0, false, false));
+				entity.clearFire();
+			} else if (entity.isInWaterOrBubble()) {
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.WATER_BREATHING, 400, 0, false, false));
+			}
+		}
+	}
+}
