e756b49b885ac0ee9add48ed5e7fe748c1720be7
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-03-11 00:03:04 +0300

    Tests for new abilities wheel

--- src/main/java/com/esmods/keepersofthestonestwo/client/gui/WheelAbilitiesCrystalScreen.java
+++ src/main/java/com/esmods/keepersofthestonestwo/client/gui/WheelAbilitiesCrystalScreen.java
@@ -38,8 +38,8 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		this.y = container.y;
 		this.z = container.z;
 		this.entity = container.entity;
-		this.imageWidth = 192;
-		this.imageHeight = 192;
+		this.imageWidth = 193;
+		this.imageHeight = 201;
 	}
 
 	@Override
@@ -47,11 +47,11 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		this.renderBackground(guiGraphics);
 		super.render(guiGraphics, mouseX, mouseY, partialTicks);
 		this.renderTooltip(guiGraphics, mouseX, mouseY);
-		if (mouseX > leftPos + 82 && mouseX < leftPos + 106 && mouseY > topPos + 22 && mouseY < topPos + 46)
+		if (mouseX > leftPos + 84 && mouseX < leftPos + 108 && mouseY > topPos + 23 && mouseY < topPos + 47)
 			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_crystal.tooltip_crystal_block_attack_uses_10"), mouseX, mouseY);
-		if (mouseX > leftPos + 145 && mouseX < leftPos + 169 && mouseY > topPos + 83 && mouseY < topPos + 107)
+		if (mouseX > leftPos + 137 && mouseX < leftPos + 161 && mouseY > topPos + 77 && mouseY < topPos + 101)
 			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_crystal.tooltip_crystal_piercing_uses_40"), mouseX, mouseY);
-		if (mouseX > leftPos + 82 && mouseX < leftPos + 106 && mouseY > topPos + 144 && mouseY < topPos + 168)
+		if (mouseX > leftPos + 84 && mouseX < leftPos + 108 && mouseY > topPos + 130 && mouseY < topPos + 154)
 			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_crystal.tooltip_crystal_volley_uses_65"), mouseX, mouseY);
 	}
 
@@ -61,7 +61,7 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		RenderSystem.enableBlend();
 		RenderSystem.defaultBlendFunc();
 
-		guiGraphics.blit(new ResourceLocation("power:textures/screens/wheel_of_abilities.png"), this.leftPos + -1, this.topPos + 0, 0, 0, 192, 192, 192, 192);
+		guiGraphics.blit(new ResourceLocation("power:textures/screens/wheel_of_abilities.png"), this.leftPos + 0, this.topPos + -4, 0, 0, 192, 205, 192, 205);
 
 		RenderSystem.disableBlend();
 	}
@@ -92,7 +92,7 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 	@Override
 	public void init() {
 		super.init();
-		imagebutton_wheel_button_1 = new ImageButton(this.leftPos + 140, this.topPos + 154, 10, 7, 0, 0, 7, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_1.png"), 10, 14, e -> {
+		imagebutton_wheel_button_1 = new ImageButton(this.leftPos + 73, this.topPos + 91, 13, 20, 0, 0, 20, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_1.png"), 13, 40, e -> {
 			if (GetWheelTwoProcedure.execute(entity)) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesCrystalButtonMessage(0, x, y, z));
 				WheelAbilitiesCrystalButtonMessage.handleButtonAction(entity, 0, x, y, z);
@@ -106,7 +106,7 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		};
 		guistate.put("button:imagebutton_wheel_button_1", imagebutton_wheel_button_1);
 		this.addRenderableWidget(imagebutton_wheel_button_1);
-		imagebutton_wheel_button_2 = new ImageButton(this.leftPos + 152, this.topPos + 154, 10, 7, 0, 0, 7, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_2.png"), 10, 14, e -> {
+		imagebutton_wheel_button_2 = new ImageButton(this.leftPos + 89, this.topPos + 89, 14, 20, 0, 0, 20, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_2.png"), 14, 40, e -> {
 			if (GetWheelTwoProcedure.execute(entity)) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesCrystalButtonMessage(1, x, y, z));
 				WheelAbilitiesCrystalButtonMessage.handleButtonAction(entity, 1, x, y, z);
@@ -120,7 +120,7 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		};
 		guistate.put("button:imagebutton_wheel_button_2", imagebutton_wheel_button_2);
 		this.addRenderableWidget(imagebutton_wheel_button_2);
-		imagebutton_wheel_button_3 = new ImageButton(this.leftPos + 164, this.topPos + 154, 10, 7, 0, 0, 7, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_3.png"), 10, 14, e -> {
+		imagebutton_wheel_button_3 = new ImageButton(this.leftPos + 106, this.topPos + 92, 13, 20, 0, 0, 20, new ResourceLocation("power:textures/screens/atlas/imagebutton_wheel_button_3.png"), 13, 40, e -> {
 			if (GetWheelThreeProcedure.execute(entity)) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesCrystalButtonMessage(2, x, y, z));
 				WheelAbilitiesCrystalButtonMessage.handleButtonAction(entity, 2, x, y, z);
@@ -134,7 +134,7 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		};
 		guistate.put("button:imagebutton_wheel_button_3", imagebutton_wheel_button_3);
 		this.addRenderableWidget(imagebutton_wheel_button_3);
-		imagebutton_crystal_block_attack = new ImageButton(this.leftPos + 72, this.topPos + 12, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_crystal_block_attack.png"), 46, 92, e -> {
+		imagebutton_crystal_block_attack = new ImageButton(this.leftPos + 73, this.topPos + 12, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_crystal_block_attack.png"), 46, 92, e -> {
 			if (true) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesCrystalButtonMessage(3, x, y, z));
 				WheelAbilitiesCrystalButtonMessage.handleButtonAction(entity, 3, x, y, z);
@@ -142,7 +142,7 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		});
 		guistate.put("button:imagebutton_crystal_block_attack", imagebutton_crystal_block_attack);
 		this.addRenderableWidget(imagebutton_crystal_block_attack);
-		imagebutton_crystal_piercing = new ImageButton(this.leftPos + 133, this.topPos + 73, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_crystal_piercing.png"), 46, 92, e -> {
+		imagebutton_crystal_piercing = new ImageButton(this.leftPos + 128, this.topPos + 66, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_crystal_piercing.png"), 46, 92, e -> {
 			if (true) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesCrystalButtonMessage(4, x, y, z));
 				WheelAbilitiesCrystalButtonMessage.handleButtonAction(entity, 4, x, y, z);
@@ -150,7 +150,7 @@ public class WheelAbilitiesCrystalScreen extends AbstractContainerScreen<WheelAb
 		});
 		guistate.put("button:imagebutton_crystal_piercing", imagebutton_crystal_piercing);
 		this.addRenderableWidget(imagebutton_crystal_piercing);
-		imagebutton_crystal_volley = new ImageButton(this.leftPos + 72, this.topPos + 134, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_crystal_volley.png"), 46, 92, e -> {
+		imagebutton_crystal_volley = new ImageButton(this.leftPos + 73, this.topPos + 119, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_crystal_volley.png"), 46, 92, e -> {
 			if (true) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesCrystalButtonMessage(5, x, y, z));
 				WheelAbilitiesCrystalButtonMessage.handleButtonAction(entity, 5, x, y, z);
