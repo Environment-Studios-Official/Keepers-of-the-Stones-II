f1c48921fa0676ca0d20c0f91521575db09461c1
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-03-22 12:19:35 +0300

    More fixes

--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModTabs.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModTabs.java
@@ -42,7 +42,6 @@ public class PowerModTabs {
 				tabData.accept(PowerModItems.MOON_STONE.get());
 				tabData.accept(PowerModItems.SPACE_STONE.get());
 				tabData.accept(PowerModItems.TIME_STONE.get());
-				tabData.accept(PowerModItems.CREATION_STONE.get());
 			})
 
 					.build());
