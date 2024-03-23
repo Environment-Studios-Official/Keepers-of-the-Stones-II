58a71b958312d2f009eba507d45abc25d94fe9aa
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-03-23 22:02:46 +0300

    Added weapon for creation element

--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
@@ -146,6 +146,7 @@ import com.esmods.keepersofthestonestwo.item.CrystalKnifeItem;
 import com.esmods.keepersofthestonestwo.item.CrystalBatteryItem;
 import com.esmods.keepersofthestonestwo.item.CrystalArmorItem;
 import com.esmods.keepersofthestonestwo.item.CreationStoneItem;
+import com.esmods.keepersofthestonestwo.item.CreationStaffItem;
 import com.esmods.keepersofthestonestwo.item.CreationBatteryItem;
 import com.esmods.keepersofthestonestwo.item.CreationArmorItem;
 import com.esmods.keepersofthestonestwo.item.ConstellationSwordItem;
@@ -391,6 +392,7 @@ public class PowerModItems {
 	public static final RegistryObject<Item> CREATION_ARMOR_CHESTPLATE = REGISTRY.register("creation_armor_chestplate", () -> new CreationArmorItem.Chestplate());
 	public static final RegistryObject<Item> CREATION_ARMOR_LEGGINGS = REGISTRY.register("creation_armor_leggings", () -> new CreationArmorItem.Leggings());
 	public static final RegistryObject<Item> CREATION_ARMOR_BOOTS = REGISTRY.register("creation_armor_boots", () -> new CreationArmorItem.Boots());
+	public static final RegistryObject<Item> CREATION_STAFF = REGISTRY.register("creation_staff", () -> new CreationStaffItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
