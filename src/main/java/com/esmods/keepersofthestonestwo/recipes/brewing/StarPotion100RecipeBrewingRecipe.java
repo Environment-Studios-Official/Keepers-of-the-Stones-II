8d47a6d3dd334502e0f6acf6c52bcaf5608becf2
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-03-31 22:37:13 +0300

    Added star potion crafts

---
+++ src/main/java/com/esmods/keepersofthestonestwo/recipes/brewing/StarPotion100RecipeBrewingRecipe.java
@@ -0,0 +1,39 @@
+
+package com.esmods.keepersofthestonestwo.recipes.brewing;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.ItemStack;
+
+import com.esmods.keepersofthestonestwo.init.PowerModItems;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class StarPotion100RecipeBrewingRecipe implements IBrewingRecipe {
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(new StarPotion100RecipeBrewingRecipe()));
+	}
+
+	@Override
+	public boolean isInput(ItemStack input) {
+		return Ingredient.of(new ItemStack(PowerModItems.STAR_POTION_50.get())).test(input);
+	}
+
+	@Override
+	public boolean isIngredient(ItemStack ingredient) {
+		return Ingredient.of(new ItemStack(PowerModItems.AMPLIFIER_DROP.get())).test(ingredient);
+	}
+
+	@Override
+	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+		if (isInput(input) && isIngredient(ingredient)) {
+			return new ItemStack(PowerModItems.STAR_POTION_100.get());
+		}
+		return ItemStack.EMPTY;
+	}
+}
