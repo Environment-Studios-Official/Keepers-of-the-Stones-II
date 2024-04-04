d593fa51f651f7188411fd3bf83a7212bd074435
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-04-04 16:51:53 +0300

    Some fixes

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -3,3 +3,4 @@ What's new in Keepers of the Stones II version 1.0.0.17?<br />
 <br />- The amplifier drop can now be thrown
 <br />- When an amplifer drop hits a player, the player can get from 0 to 20 power points
 <br />- [Bugfix] Added missing crafts for amplifier
+<br />- [Bugfix] Fixed a bug where stopping time did not affect movement when jumping
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/TimeStoppedKazhdyiTikVoVriemiaEffiektaProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/TimeStoppedKazhdyiTikVoVriemiaEffiektaProcedure.java
@@ -1,5 +1,7 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
@@ -13,5 +15,6 @@ public class TimeStoppedKazhdyiTikVoVriemiaEffiektaProcedure {
 			_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 200, 200, false, false));
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 			_entity.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 200, 200, false, false));
+		entity.makeStuckInBlock(Blocks.AIR.defaultBlockState(), new Vec3(0.25, 0.05, 0.25));
 	}
 }
