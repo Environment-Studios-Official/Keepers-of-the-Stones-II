ef55f0af4e596d142e5cd4b1034368bdeb643b3e
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-04-24 23:20:18 +0300

    Some changes

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/SpecialAttackProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/SpecialAttackProcedure.java
@@ -44,8 +44,6 @@ import net.minecraft.commands.CommandSource;
 import java.util.List;
 import java.util.Comparator;
 
-import dev.kosmx.playerAnim.core.data.quarktool.Reset;
-
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
 import com.esmods.keepersofthestonestwo.init.PowerModParticleTypes;
 import com.esmods.keepersofthestonestwo.init.PowerModMobEffects;
