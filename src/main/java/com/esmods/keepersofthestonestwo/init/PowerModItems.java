2ecb4b18e8441f79a0d6ec2e73f5e51f49746fb6
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-05-15 01:17:32 +0300

    Added weapon for explosion element

--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
@@ -123,6 +123,7 @@ import com.esmods.keepersofthestonestwo.item.FireStoneItem;
 import com.esmods.keepersofthestonestwo.item.FireBatteryItem;
 import com.esmods.keepersofthestonestwo.item.FireArmorItem;
 import com.esmods.keepersofthestonestwo.item.ExplosionStoneItem;
+import com.esmods.keepersofthestonestwo.item.ExplosionMaceItem;
 import com.esmods.keepersofthestonestwo.item.ExplosionBatteryItem;
 import com.esmods.keepersofthestonestwo.item.ExplosionArmorItem;
 import com.esmods.keepersofthestonestwo.item.EtherStoneItem;
@@ -458,6 +459,7 @@ public class PowerModItems {
 	public static final RegistryObject<Item> EXPLOSION_ARMOR_LEGGINGS = REGISTRY.register("explosion_armor_leggings", () -> new ExplosionArmorItem.Leggings());
 	public static final RegistryObject<Item> EXPLOSION_ARMOR_BOOTS = REGISTRY.register("explosion_armor_boots", () -> new ExplosionArmorItem.Boots());
 	public static final RegistryObject<Item> EXPLOSION_BATTERY = REGISTRY.register("explosion_battery", () -> new ExplosionBatteryItem());
+	public static final RegistryObject<Item> EXPLOSION_MACE = REGISTRY.register("explosion_mace", () -> new ExplosionMaceItem());
 
 	// Start of user code block custom items
 	// End of user code block custom items
