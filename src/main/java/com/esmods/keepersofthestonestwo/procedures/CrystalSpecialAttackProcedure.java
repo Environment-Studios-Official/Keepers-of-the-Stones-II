f4c5964891fb2ec444b51df20aac444acd36c27c
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-05-18 19:39:21 +0300

    Increased the speed and height of projectiles for abilities that strike at a radius

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/CrystalSpecialAttackProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/CrystalSpecialAttackProcedure.java
@@ -142,8 +142,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot((-1), 0, (-1), 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot((-1), 0, (-1), (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				if (world instanceof ServerLevel projectileLevel) {
@@ -158,8 +158,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot(1, 0, 1, 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot(1, 0, 1, (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				if (world instanceof ServerLevel projectileLevel) {
@@ -174,8 +174,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot((-1), 0, 1, 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot((-1), 0, 1, (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				if (world instanceof ServerLevel projectileLevel) {
@@ -190,8 +190,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot(1, 0, (-1), 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot(1, 0, (-1), (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				if (world instanceof ServerLevel projectileLevel) {
@@ -206,8 +206,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot(0, 0, (-1), 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot(0, 0, (-1), (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				if (world instanceof ServerLevel projectileLevel) {
@@ -222,8 +222,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot(0, 0, 1, 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot(0, 0, 1, (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				if (world instanceof ServerLevel projectileLevel) {
@@ -238,8 +238,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot((-1), 0, 0, 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot((-1), 0, 0, (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				if (world instanceof ServerLevel projectileLevel) {
@@ -254,8 +254,8 @@ public class CrystalSpecialAttackProcedure {
 							return entityToSpawn;
 						}
 					}.getArrow(projectileLevel, entity, (float) 22.5, 0, (byte) 3);
-					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 2), z);
-					_entityToSpawn.shoot(1, 0, 0, 1, 0);
+					_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
+					_entityToSpawn.shoot(1, 0, 0, (float) 1.5, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 				{
