4a64a060c15a07aa0de762fb3544448d2d419fd7
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-05-27 23:27:08 +0300

    Trying fix

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/RechargeStoneTickEventProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/RechargeStoneTickEventProcedure.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.ItemStack;
 
 public class RechargeStoneTickEventProcedure {
 	public static void execute(ItemStack itemstack) {
-		itemstack.getOrCreateTag().putDouble("rechargeStone", (itemstack.getOrCreateTag().getDouble("rechargeStone") > 0 ? itemstack.getOrCreateTag().getDouble("rechargeStone") - 1 : 0));
+		itemstack.getOrCreateTag().putString("rechargeStone", "Placeholder");
+		itemstack.getOrCreateTag().putDouble("rechargeStone", (itemstack.getOrCreateTag().getDouble("rechargeStone") - 1));
 	}
 }
