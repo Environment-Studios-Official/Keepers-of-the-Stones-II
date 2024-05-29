980c1c4856d34b82ab86eece2407eb52e56430bc
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-05-30 00:37:46 +0300

    Some fixes

--- src/main/java/com/esmods/keepersofthestonestwo/block/CursedLanternBlock.java
+++ src/main/java/com/esmods/keepersofthestonestwo/block/CursedLanternBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -19,18 +20,27 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 import com.esmods.keepersofthestonestwo.procedures.CursedLanternTopProcedure;
+import com.esmods.keepersofthestonestwo.procedures.CursedLanternAfterAddedProcedure;
 
 public class CursedLanternBlock extends Block implements SimpleWaterloggedBlock {
+	public static final IntegerProperty BLOCKSTATE = IntegerProperty.create("blockstate", 0, 1);
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
 	public CursedLanternBlock() {
-		super(BlockBehaviour.Properties.of().sound(SoundType.TUFF).strength(2.5f, 3f).lightLevel(s -> 13).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+		super(BlockBehaviour.Properties.of().sound(SoundType.TUFF).strength(2.5f, 3f).lightLevel(s -> (new Object() {
+			public int getLightLevel() {
+				if (s.getValue(BLOCKSTATE) == 1)
+					return 13;
+				return 13;
+			}
+		}.getLightLevel())).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
 		this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
 	}
 
@@ -51,13 +61,17 @@ public class CursedLanternBlock extends Block implements SimpleWaterloggedBlock 
 
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		if (state.getValue(BLOCKSTATE) == 1) {
+			return Shapes.or(box(6, 3, 6, 10, 8, 10), box(5, 8, 6, 11, 10, 10), box(6, 10, 6, 10, 11, 10), box(6, 0, 6, 10, 1, 10), box(6, 8, 5, 10, 10, 6), box(7, 7, 5, 9, 8, 6), box(7, 3, 5, 9, 4, 6), box(7, 7, 10, 9, 8, 11),
+					box(7, 3, 10, 9, 4, 11), box(10, 7, 7, 11, 8, 9), box(10, 3, 7, 11, 4, 9), box(5, 7, 7, 6, 8, 9), box(5, 3, 7, 6, 4, 9), box(6, 8, 10, 10, 10, 11), box(6, 1, 10, 10, 3, 11), box(6, 1, 5, 10, 3, 6), box(5, 1, 6, 11, 3, 10));
+		}
 		return Shapes.or(box(6, 3, 6, 10, 8, 10), box(5, 8, 6, 11, 10, 10), box(6, 10, 6, 10, 11, 10), box(6, 0, 6, 10, 1, 10), box(6, 8, 5, 10, 10, 6), box(7, 7, 5, 9, 8, 6), box(7, 3, 5, 9, 4, 6), box(7, 7, 10, 9, 8, 11), box(7, 3, 10, 9, 4, 11),
 				box(10, 7, 7, 11, 8, 9), box(10, 3, 7, 11, 4, 9), box(5, 7, 7, 6, 8, 9), box(5, 3, 7, 6, 4, 9), box(6, 8, 10, 10, 10, 11), box(6, 1, 10, 10, 3, 11), box(6, 1, 5, 10, 3, 6), box(5, 1, 6, 11, 3, 10));
 	}
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(WATERLOGGED);
+		builder.add(WATERLOGGED, BLOCKSTATE);
 	}
 
 	@Override
@@ -88,5 +102,11 @@ public class CursedLanternBlock extends Block implements SimpleWaterloggedBlock 
 			world.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
 		}
 		return !state.canSurvive(world, currentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, facing, facingState, world, currentPos, facingPos);
+	}
+
+	@Override
+	public void onPlace(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
+		super.onPlace(blockstate, world, pos, oldState, moving);
+		CursedLanternAfterAddedProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
 	}
 }
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/CursedLanternTopProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/CursedLanternTopProcedure.java
@@ -1,11 +1,16 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 public class CursedLanternTopProcedure {
 	public static boolean execute(LevelAccessor world, double x, double y, double z) {
-		return world.getBlockState(BlockPos.containing(x, y + 1, z)).canOcclude() && world.getBlockState(BlockPos.containing(x, y + 1, z)).isFaceSturdy(world, BlockPos.containing(x, y + 1, z), Direction.DOWN);
+		return world.getBlockState(BlockPos.containing(x, y + 1, z)).isFaceSturdy(world, BlockPos.containing(x, y + 1, z), Direction.DOWN)
+				^ (world.getBlockState(BlockPos.containing(x, y + 1, z))).is(BlockTags.create(new ResourceLocation("minecraft:fences")))
+				|| world.getBlockState(BlockPos.containing(x, y - 1, z)).isFaceSturdy(world, BlockPos.containing(x, y - 1, z), Direction.UP)
+						^ (world.getBlockState(BlockPos.containing(x, y - 1, z))).is(BlockTags.create(new ResourceLocation("minecraft:fences")));
 	}
 }
