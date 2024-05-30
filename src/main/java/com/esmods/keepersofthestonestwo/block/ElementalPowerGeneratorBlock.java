ef8b253be329f5192b85270a75f2a8238ec14a95
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-05-30 23:38:58 +0300

    Added new apis in jar-jar

--- src/main/java/com/esmods/keepersofthestonestwo/block/ElementalPowerGeneratorBlock.java
+++ src/main/java/com/esmods/keepersofthestonestwo/block/ElementalPowerGeneratorBlock.java
@@ -3,14 +3,21 @@ package com.esmods.keepersofthestonestwo.block;
 
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.Containers;
 import net.minecraft.core.BlockPos;
 
-public class ElementalPowerGeneratorBlock extends Block {
+import com.esmods.keepersofthestonestwo.block.entity.ElementalPowerGeneratorBlockEntity;
+
+public class ElementalPowerGeneratorBlock extends Block implements EntityBlock {
 	public ElementalPowerGeneratorBlock() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(5f).requiresCorrectToolForDrops());
 	}
@@ -25,5 +32,35 @@ public class ElementalPowerGeneratorBlock extends Block {
 		if (player.getInventory().getSelected().getItem() instanceof PickaxeItem tieredItem)
 			return tieredItem.getTier().getLevel() >= 2;
 		return false;
+	}
+
+	@Override
+	public MenuProvider getMenuProvider(BlockState state, Level worldIn, BlockPos pos) {
+		BlockEntity tileEntity = worldIn.getBlockEntity(pos);
+		return tileEntity instanceof MenuProvider menuProvider ? menuProvider : null;
+	}
+
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new ElementalPowerGeneratorBlockEntity(pos, state);
+	}
+
+	@Override
+	public boolean triggerEvent(BlockState state, Level world, BlockPos pos, int eventID, int eventParam) {
+		super.triggerEvent(state, world, pos, eventID, eventParam);
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		return blockEntity == null ? false : blockEntity.triggerEvent(eventID, eventParam);
+	}
+
+	@Override
+	public void onRemove(BlockState state, Level world, BlockPos pos, BlockState newState, boolean isMoving) {
+		if (state.getBlock() != newState.getBlock()) {
+			BlockEntity blockEntity = world.getBlockEntity(pos);
+			if (blockEntity instanceof ElementalPowerGeneratorBlockEntity be) {
+				Containers.dropContents(world, pos, be);
+				world.updateNeighbourForOutputSignal(pos, this);
+			}
+			super.onRemove(state, world, pos, newState, isMoving);
+		}
 	}
 }
--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModBlockEntities.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModBlockEntities.java
@@ -11,12 +11,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.Block;
 
+import com.esmods.keepersofthestonestwo.block.entity.ElementalPowerGeneratorBlockEntity;
 import com.esmods.keepersofthestonestwo.block.entity.BatteryChargerBlockEntity;
 import com.esmods.keepersofthestonestwo.PowerMod;
 
 public class PowerModBlockEntities {
 	public static final DeferredRegister<BlockEntityType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, PowerMod.MODID);
 	public static final RegistryObject<BlockEntityType<?>> BATTERY_CHARGER = register("battery_charger", PowerModBlocks.BATTERY_CHARGER, BatteryChargerBlockEntity::new);
+	public static final RegistryObject<BlockEntityType<?>> ELEMENTAL_POWER_GENERATOR = register("elemental_power_generator", PowerModBlocks.ELEMENTAL_POWER_GENERATOR, ElementalPowerGeneratorBlockEntity::new);
 
 	private static RegistryObject<BlockEntityType<?>> register(String registryname, RegistryObject<Block> block, BlockEntityType.BlockEntitySupplier<?> supplier) {
 		return REGISTRY.register(registryname, () -> BlockEntityType.Builder.of(supplier, block.get()).build(null));
