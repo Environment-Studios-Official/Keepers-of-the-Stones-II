3aa7a8415561d5ab62e3cc0f10f5f805ca95ce4e
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-06-05 23:52:49 +0300

    Some fixes

--- src/main/java/com/esmods/keepersofthestonestwo/block/DepletedEnergiumOreBlock.java
+++ src/main/java/com/esmods/keepersofthestonestwo/block/DepletedEnergiumOreBlock.java
@@ -1,13 +1,7 @@
 
 package com.esmods.keepersofthestonestwo.block;
 
-import org.checkerframework.checker.units.qual.s;
-
-import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
@@ -18,33 +12,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.core.BlockPos;
 
 public class DepletedEnergiumOreBlock extends Block {
-	public static final IntegerProperty BLOCKSTATE = IntegerProperty.create("blockstate", 0, 2);
-
 	public DepletedEnergiumOreBlock() {
-		super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.STONE).strength(6.75f, 5f).lightLevel(s -> (new Object() {
-			public int getLightLevel() {
-				if (s.getValue(BLOCKSTATE) == 1)
-					return 12;
-				if (s.getValue(BLOCKSTATE) == 2)
-					return 12;
-				return 0;
-			}
-		}.getLightLevel())).requiresCorrectToolForDrops());
+		super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.STONE).strength(6.75f, 5f).requiresCorrectToolForDrops());
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 15;
-	}
-
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return box(0, 0, 0, 16, 16, 16);
-	}
-
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(BLOCKSTATE);
 	}
 
 	@Override
