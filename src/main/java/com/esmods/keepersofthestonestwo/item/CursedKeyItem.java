dc551687823071dc9db45bb7cc4a83d702ccf7a0
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-06-07 12:53:41 +0300

    Disenchant key

--- src/main/java/com/esmods/keepersofthestonestwo/item/CursedKeyItem.java
+++ src/main/java/com/esmods/keepersofthestonestwo/item/CursedKeyItem.java
@@ -1,12 +1,16 @@
 
 package com.esmods.keepersofthestonestwo.item;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionResult;
 
 import com.esmods.keepersofthestonestwo.procedures.CursedKeyOpenVaultProcedure;
+import com.esmods.keepersofthestonestwo.procedures.CursedKeyKazhdyiTikVInvientarieProcedure;
 
 public class CursedKeyItem extends Item {
 	public CursedKeyItem() {
@@ -18,5 +22,11 @@ public class CursedKeyItem extends Item {
 		super.useOn(context);
 		CursedKeyOpenVaultProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getPlayer(), context.getItemInHand());
 		return InteractionResult.SUCCESS;
+	}
+
+	@Override
+	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(itemstack, world, entity, slot, selected);
+		CursedKeyKazhdyiTikVInvientarieProcedure.execute(itemstack);
 	}
 }
