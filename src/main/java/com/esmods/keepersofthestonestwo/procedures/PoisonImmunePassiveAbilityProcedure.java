95d8323bf5dad5cdf86ce45af1ca497e14051b24
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-06-10 23:40:02 +0300

    Fixes

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/PoisonImmunePassiveAbilityProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/PoisonImmunePassiveAbilityProcedure.java
@@ -1,5 +1,10 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
@@ -8,11 +13,25 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.util.Mth;
 import net.minecraft.core.particles.SimpleParticleType;
 
+import javax.annotation.Nullable;
+
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
 import com.esmods.keepersofthestonestwo.init.PowerModParticleTypes;
 
+@Mod.EventBusSubscriber
 public class PoisonImmunePassiveAbilityProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player.level(), event.player.getX(), event.player.getY(), event.player.getZ(), event.player);
+		}
+	}
+
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		double Scaling = 0;
