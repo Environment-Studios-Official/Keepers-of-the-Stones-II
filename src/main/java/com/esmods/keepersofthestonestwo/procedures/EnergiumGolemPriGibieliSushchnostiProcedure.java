31f514e57004a4ab1cf9cdb3c367a0881022fac9
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-06-15 23:52:55 +0300

    Some fixes

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/EnergiumGolemPriGibieliSushchnostiProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/EnergiumGolemPriGibieliSushchnostiProcedure.java
@@ -16,7 +16,7 @@ public class EnergiumGolemPriGibieliSushchnostiProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		{
 			final Vec3 _center = new Vec3(x, y, z);
-			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(16 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(64 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 			for (Entity entityiterator : _entfound) {
 				if (entityiterator instanceof ServerPlayer _player) {
 					Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("power:the_end_of_tyranny"));
