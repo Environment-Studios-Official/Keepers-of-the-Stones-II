bf19ec2e0bdf893cba6a3cad0ccffa83335aa382
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-06-16 22:14:46 +0300

    Added icons for abilities magnet element

--- src/main/java/com/esmods/keepersofthestonestwo/client/gui/WheelAbilitiesMagnetScreen.java
+++ src/main/java/com/esmods/keepersofthestonestwo/client/gui/WheelAbilitiesMagnetScreen.java
@@ -34,9 +34,9 @@ public class WheelAbilitiesMagnetScreen extends AbstractContainerScreen<WheelAbi
 	ImageButton imagebutton_fake_wheel_button_1;
 	ImageButton imagebutton_fake_wheel_button_2;
 	ImageButton imagebutton_fake_wheel_button_3;
-	ImageButton imagebutton_ability_1;
-	ImageButton imagebutton_ability_2;
-	ImageButton imagebutton_ability_3;
+	ImageButton imagebutton_magnetic_field_wave;
+	ImageButton imagebutton_negative_charge;
+	ImageButton imagebutton_positive_charge;
 
 	public WheelAbilitiesMagnetScreen(WheelAbilitiesMagnetMenu container, Inventory inventory, Component text) {
 		super(container, inventory, text);
@@ -54,6 +54,12 @@ public class WheelAbilitiesMagnetScreen extends AbstractContainerScreen<WheelAbi
 		this.renderBackground(guiGraphics);
 		super.render(guiGraphics, mouseX, mouseY, partialTicks);
 		this.renderTooltip(guiGraphics, mouseX, mouseY);
+		if (mouseX > leftPos + 83 && mouseX < leftPos + 107 && mouseY > topPos + 21 && mouseY < topPos + 45)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_magnet.tooltip_magnetic_field_wave_uses_10"), mouseX, mouseY);
+		if (mouseX > leftPos + 143 && mouseX < leftPos + 167 && mouseY > topPos + 83 && mouseY < topPos + 107)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_magnet.tooltip_negative_charge_uses_30"), mouseX, mouseY);
+		if (mouseX > leftPos + 82 && mouseX < leftPos + 106 && mouseY > topPos + 145 && mouseY < topPos + 169)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_magnet.tooltip_positive_charge_uses_30"), mouseX, mouseY);
 	}
 
 	@Override
@@ -172,29 +178,29 @@ public class WheelAbilitiesMagnetScreen extends AbstractContainerScreen<WheelAbi
 		};
 		guistate.put("button:imagebutton_fake_wheel_button_3", imagebutton_fake_wheel_button_3);
 		this.addRenderableWidget(imagebutton_fake_wheel_button_3);
-		imagebutton_ability_1 = new ImageButton(this.leftPos + 72, this.topPos + 12, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_ability_1.png"), 46, 92, e -> {
+		imagebutton_magnetic_field_wave = new ImageButton(this.leftPos + 72, this.topPos + 12, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_magnetic_field_wave.png"), 46, 92, e -> {
 			if (true) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesMagnetButtonMessage(6, x, y, z));
 				WheelAbilitiesMagnetButtonMessage.handleButtonAction(entity, 6, x, y, z);
 			}
 		});
-		guistate.put("button:imagebutton_ability_1", imagebutton_ability_1);
-		this.addRenderableWidget(imagebutton_ability_1);
-		imagebutton_ability_2 = new ImageButton(this.leftPos + 133, this.topPos + 73, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_ability_2.png"), 46, 92, e -> {
+		guistate.put("button:imagebutton_magnetic_field_wave", imagebutton_magnetic_field_wave);
+		this.addRenderableWidget(imagebutton_magnetic_field_wave);
+		imagebutton_negative_charge = new ImageButton(this.leftPos + 133, this.topPos + 73, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_negative_charge.png"), 46, 92, e -> {
 			if (true) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesMagnetButtonMessage(7, x, y, z));
 				WheelAbilitiesMagnetButtonMessage.handleButtonAction(entity, 7, x, y, z);
 			}
 		});
-		guistate.put("button:imagebutton_ability_2", imagebutton_ability_2);
-		this.addRenderableWidget(imagebutton_ability_2);
-		imagebutton_ability_3 = new ImageButton(this.leftPos + 72, this.topPos + 134, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_ability_3.png"), 46, 92, e -> {
+		guistate.put("button:imagebutton_negative_charge", imagebutton_negative_charge);
+		this.addRenderableWidget(imagebutton_negative_charge);
+		imagebutton_positive_charge = new ImageButton(this.leftPos + 72, this.topPos + 134, 46, 46, 0, 0, 46, new ResourceLocation("power:textures/screens/atlas/imagebutton_positive_charge.png"), 46, 92, e -> {
 			if (true) {
 				PowerMod.PACKET_HANDLER.sendToServer(new WheelAbilitiesMagnetButtonMessage(8, x, y, z));
 				WheelAbilitiesMagnetButtonMessage.handleButtonAction(entity, 8, x, y, z);
 			}
 		});
-		guistate.put("button:imagebutton_ability_3", imagebutton_ability_3);
-		this.addRenderableWidget(imagebutton_ability_3);
+		guistate.put("button:imagebutton_positive_charge", imagebutton_positive_charge);
+		this.addRenderableWidget(imagebutton_positive_charge);
 	}
 }
