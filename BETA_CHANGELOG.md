92d32ca312888ceee7a06769da67ae53a968ad39
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-06-30 23:39:23 +0300

    The batteries have been moved to a separate creative mode tab

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,4 +1,5 @@
 What's new in Keepers of the Stones II version 1.0.0.29?<br />
 <br />- The generation of the cursed forest biome has been temporarily removed, as the location is not in use and needs to be improved
+<br />- The batteries have been moved to a separate creative mode tab
 <br />- [Bugfix] Fixed the id and name of the music disc
 <br />- [Bugfix] Fixed a bug where the earthquake ability could destroy items
--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModTabs.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModTabs.java
@@ -17,7 +17,7 @@ import com.esmods.keepersofthestonestwo.PowerMod;
 public class PowerModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PowerMod.MODID);
 	public static final RegistryObject<CreativeModeTab> ITEMS = REGISTRY.register("items",
-			() -> CreativeModeTab.builder().title(Component.translatable("item_group.power.items")).icon(() -> new ItemStack(PowerModItems.EMPTY_BATTERY.get())).displayItems((parameters, tabData) -> {
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.power.items")).icon(() -> new ItemStack(PowerModItems.AMPLIFIER_RING.get())).displayItems((parameters, tabData) -> {
 				tabData.accept(PowerModBlocks.AMPLIFIER_ORE.get().asItem());
 				tabData.accept(PowerModBlocks.RAW_AMPLIFIER_BLOCK.get().asItem());
 				tabData.accept(PowerModBlocks.AMPLIFIER_BLOCK.get().asItem());
@@ -72,44 +72,6 @@ public class PowerModTabs {
 				tabData.accept(PowerModBlocks.CURSED_LANTERN.get().asItem());
 				tabData.accept(PowerModBlocks.CURSED_VAULT.get().asItem());
 				tabData.accept(PowerModItems.CURSED_KEY.get());
-				tabData.accept(PowerModItems.EMPTY_BATTERY.get());
-				tabData.accept(PowerModItems.FIRE_BATTERY.get());
-				tabData.accept(PowerModItems.AIR_BATTERY.get());
-				tabData.accept(PowerModItems.EARTH_BATTERY.get());
-				tabData.accept(PowerModItems.WATER_BATTERY.get());
-				tabData.accept(PowerModItems.ETHER_BATTERY.get());
-				tabData.accept(PowerModItems.ICE_BATTERY.get());
-				tabData.accept(PowerModItems.LIGHTNING_BATTERY.get());
-				tabData.accept(PowerModItems.SOUND_BATTERY.get());
-				tabData.accept(PowerModItems.CRYSTAL_BATTERY.get());
-				tabData.accept(PowerModItems.LAVA_BATTERY.get());
-				tabData.accept(PowerModItems.RAIN_BATTERY.get());
-				tabData.accept(PowerModItems.TORNADO_BATTERY.get());
-				tabData.accept(PowerModItems.OCEAN_BATTERY.get());
-				tabData.accept(PowerModItems.PLANTS_BATTERY.get());
-				tabData.accept(PowerModItems.ANIMALS_BATTERY.get());
-				tabData.accept(PowerModItems.METAL_BATTERY.get());
-				tabData.accept(PowerModItems.LIGHT_BATTERY.get());
-				tabData.accept(PowerModItems.SHADOW_BATTERY.get());
-				tabData.accept(PowerModItems.VACUUM_BATTERY.get());
-				tabData.accept(PowerModItems.ENERGY_BATTERY.get());
-				tabData.accept(PowerModItems.SUN_BATTERY.get());
-				tabData.accept(PowerModItems.MOON_BATTERY.get());
-				tabData.accept(PowerModItems.SPACE_BATTERY.get());
-				tabData.accept(PowerModItems.TIME_BATTERY.get());
-				tabData.accept(PowerModItems.CREATION_BATTERY.get());
-				tabData.accept(PowerModItems.DESTRUCTION_BATTERY.get());
-				tabData.accept(PowerModItems.BLOOD_BATTERY.get());
-				tabData.accept(PowerModItems.TECHNOLOGY_BATTERY.get());
-				tabData.accept(PowerModItems.TELEPORTATION_BATTERY.get());
-				tabData.accept(PowerModItems.EXPLOSION_BATTERY.get());
-				tabData.accept(PowerModItems.AMBER_BATTERY.get());
-				tabData.accept(PowerModItems.MIST_BATTERY.get());
-				tabData.accept(PowerModItems.SAND_BATTERY.get());
-				tabData.accept(PowerModItems.SPEED_BATTERY.get());
-				tabData.accept(PowerModItems.POISON_BATTERY.get());
-				tabData.accept(PowerModItems.MAGNET_BATTERY.get());
-				tabData.accept(PowerModItems.MUSHROOMS_BATTERY.get());
 			})
 
 					.build());
@@ -158,6 +120,49 @@ public class PowerModTabs {
 	public static final RegistryObject<CreativeModeTab> MOBS = REGISTRY.register("mobs",
 			() -> CreativeModeTab.builder().title(Component.translatable("item_group.power.mobs")).icon(() -> new ItemStack(PowerModItems.ENERGIUM_GOLEM_SPAWN_EGG.get())).displayItems((parameters, tabData) -> {
 				tabData.accept(PowerModItems.ENERGIUM_GOLEM_SPAWN_EGG.get());
+			})
+
+					.build());
+	public static final RegistryObject<CreativeModeTab> BATTERIES = REGISTRY.register("batteries",
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.power.batteries")).icon(() -> new ItemStack(PowerModItems.EMPTY_BATTERY.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(PowerModItems.EMPTY_BATTERY.get());
+				tabData.accept(PowerModItems.FIRE_BATTERY.get());
+				tabData.accept(PowerModItems.AIR_BATTERY.get());
+				tabData.accept(PowerModItems.EARTH_BATTERY.get());
+				tabData.accept(PowerModItems.WATER_BATTERY.get());
+				tabData.accept(PowerModItems.ETHER_BATTERY.get());
+				tabData.accept(PowerModItems.ICE_BATTERY.get());
+				tabData.accept(PowerModItems.LIGHTNING_BATTERY.get());
+				tabData.accept(PowerModItems.SOUND_BATTERY.get());
+				tabData.accept(PowerModItems.CRYSTAL_BATTERY.get());
+				tabData.accept(PowerModItems.LAVA_BATTERY.get());
+				tabData.accept(PowerModItems.RAIN_BATTERY.get());
+				tabData.accept(PowerModItems.TORNADO_BATTERY.get());
+				tabData.accept(PowerModItems.OCEAN_BATTERY.get());
+				tabData.accept(PowerModItems.PLANTS_BATTERY.get());
+				tabData.accept(PowerModItems.ANIMALS_BATTERY.get());
+				tabData.accept(PowerModItems.METAL_BATTERY.get());
+				tabData.accept(PowerModItems.LIGHT_BATTERY.get());
+				tabData.accept(PowerModItems.SHADOW_BATTERY.get());
+				tabData.accept(PowerModItems.VACUUM_BATTERY.get());
+				tabData.accept(PowerModItems.ENERGY_BATTERY.get());
+				tabData.accept(PowerModItems.SUN_BATTERY.get());
+				tabData.accept(PowerModItems.MOON_BATTERY.get());
+				tabData.accept(PowerModItems.SPACE_BATTERY.get());
+				tabData.accept(PowerModItems.TIME_BATTERY.get());
+				tabData.accept(PowerModItems.CREATION_BATTERY.get());
+				tabData.accept(PowerModItems.DESTRUCTION_BATTERY.get());
+				tabData.accept(PowerModItems.BLOOD_BATTERY.get());
+				tabData.accept(PowerModItems.TECHNOLOGY_BATTERY.get());
+				tabData.accept(PowerModItems.TELEPORTATION_BATTERY.get());
+				tabData.accept(PowerModItems.EXPLOSION_BATTERY.get());
+				tabData.accept(PowerModItems.AMBER_BATTERY.get());
+				tabData.accept(PowerModItems.MIST_BATTERY.get());
+				tabData.accept(PowerModItems.SAND_BATTERY.get());
+				tabData.accept(PowerModItems.SPEED_BATTERY.get());
+				tabData.accept(PowerModItems.POISON_BATTERY.get());
+				tabData.accept(PowerModItems.MAGNET_BATTERY.get());
+				tabData.accept(PowerModItems.MUSHROOMS_BATTERY.get());
 			})
 
 					.build());
