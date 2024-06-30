8248b0a92bf649095ee98eeabbc449b2149a051e
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-06-30 23:54:21 +0300

    Added sounds for the elemental power generator from the beacon

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,5 +1,6 @@
 What's new in Keepers of the Stones II version 1.0.0.29?<br />
 <br />- The generation of the cursed forest biome has been temporarily removed, as the location is not in use and needs to be improved
 <br />- The batteries have been moved to a separate creative mode tab
+<br />- Added sounds for the elemental power generator from the beacon
 <br />- [Bugfix] Fixed the id and name of the music disc
 <br />- [Bugfix] Fixed a bug where the earthquake ability could destroy items
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/ElementalPowerGeneratorSaveElementalPowerProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/ElementalPowerGeneratorSaveElementalPowerProcedure.java
@@ -1,11 +1,15 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
@@ -29,6 +33,13 @@ public class ElementalPowerGeneratorSaveElementalPowerProcedure {
 								_level.sendBlockUpdated(_bp, _bs, _bs, 3);
 						}
 					});
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.activate")), SoundSource.BLOCKS, 1, 1);
+						} else {
+							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.activate")), SoundSource.BLOCKS, 1, 1, false);
+						}
+					}
 					{
 						int _value = 1;
 						BlockPos _pos = BlockPos.containing(x, y, z);
@@ -48,6 +59,13 @@ public class ElementalPowerGeneratorSaveElementalPowerProcedure {
 								_level.sendBlockUpdated(_bp, _bs, _bs, 3);
 						}
 					});
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.deactivate")), SoundSource.BLOCKS, 1, 1);
+						} else {
+							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.deactivate")), SoundSource.BLOCKS, 1, 1, false);
+						}
+					}
 					{
 						int _value = 0;
 						BlockPos _pos = BlockPos.containing(x, y, z);
@@ -97,6 +115,13 @@ public class ElementalPowerGeneratorSaveElementalPowerProcedure {
 										capability.fake_element_name_first = _setval;
 										capability.syncPlayerVariables(entity);
 									});
+								}
+								if (world instanceof Level _level) {
+									if (!_level.isClientSide()) {
+										_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1);
+									} else {
+										_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1, false);
+									}
 								}
 								{
 									double _setval = 3000;
@@ -155,6 +180,13 @@ public class ElementalPowerGeneratorSaveElementalPowerProcedure {
 										capability.syncPlayerVariables(entity);
 									});
 								}
+								if (world instanceof Level _level) {
+									if (!_level.isClientSide()) {
+										_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1);
+									} else {
+										_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1, false);
+									}
+								}
 								{
 									double _setval = 3000;
 									entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
@@ -212,6 +244,13 @@ public class ElementalPowerGeneratorSaveElementalPowerProcedure {
 										capability.fake_element_name_third = _setval;
 										capability.syncPlayerVariables(entity);
 									});
+								}
+								if (world instanceof Level _level) {
+									if (!_level.isClientSide()) {
+										_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1);
+									} else {
+										_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1, false);
+									}
 								}
 								{
 									double _setval = 3000;
