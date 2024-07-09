a37f839d237ba9e8d5d1f5f8b00f73cda215dd29
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-07-09 13:51:03 +0300

    Added missing crafts

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -3,6 +3,7 @@ What's new in Keepers of the Stones II version 1.0.0.29?<br />
 <br />- Added new game rules - limit on the number of stones and limit on getting stones for one player
 <br />- The generation of the cursed forest biome has been temporarily removed, as the location is not in use and needs to be improved
 <br />- The batteries have been moved to a separate creative mode tab
+<br />- Added missing craft for raw copyrium
 <br />- Added sounds when charging the battery in the battery charger
 <br />- Added sounds for the elemental power generator from the beacon
 <br />- Cursed and energy vaults are no longer restored after taking loot from them
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/UnlockRecipesProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/UnlockRecipesProcedure.java
@@ -112,6 +112,14 @@ public class UnlockRecipesProcedure {
 			if (entity instanceof ServerPlayer _serverPlayer)
 				_serverPlayer.awardRecipesByKey(new ResourceLocation[]{new ResourceLocation("power:raw_amplifier_block_craft")});
 		}
+		if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PowerModItems.RAW_COPYRIUM.get())) : false) {
+			if (entity instanceof ServerPlayer _serverPlayer)
+				_serverPlayer.awardRecipesByKey(new ResourceLocation[]{new ResourceLocation("power:raw_copyrium_block_craft")});
+		}
+		if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PowerModBlocks.RAW_COPYRIUM_BLOCK.get())) : false) {
+			if (entity instanceof ServerPlayer _serverPlayer)
+				_serverPlayer.awardRecipesByKey(new ResourceLocation[]{new ResourceLocation("power:raw_copyrium_from_block_craft")});
+		}
 		if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PowerModItems.COPYRIUM_INGOT.get())) : false) {
 			if (entity instanceof ServerPlayer _serverPlayer)
 				_serverPlayer.awardRecipesByKey(new ResourceLocation[]{new ResourceLocation("power:copyrium_ore_block_recipe")});
