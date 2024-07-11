f4ffd753e3e46734884a98b2fb78d3ac024b7562
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-07-12 00:04:20 +0300

    Return animations

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,3 +1,3 @@
 What's new in Keepers of the Stones II version 1.0.0.30?<br />
-<br />- 
+<br />- Added the ability to switch from Keepers of the Stones to Keepers of the Stones II (Some items may disappear during the transition)
 <br />- [Bugfix] 
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/DetransformationKeyUseProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/DetransformationKeyUseProcedure.java
@@ -1,8 +1,25 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
+import net.minecraftforge.network.NetworkDirection;
+
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.Connection;
+import net.minecraft.client.player.AbstractClientPlayer;
+
+import java.util.List;
+import java.util.Iterator;
+
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationAccess;
+import dev.kosmx.playerAnim.api.layered.ModifierLayer;
+import dev.kosmx.playerAnim.api.layered.KeyframeAnimationPlayer;
+import dev.kosmx.playerAnim.api.layered.IAnimation;
 
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
 import com.esmods.keepersofthestonestwo.init.PowerModMobEffects;
@@ -160,6 +177,27 @@ public class DetransformationKeyUseProcedure {
 					capability.attack = _setval;
 					capability.syncPlayerVariables(entity);
 				});
+			}
+			if (world.isClientSide()) {
+				if (entity instanceof AbstractClientPlayer player) {
+					var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData(player).get(new ResourceLocation("power", "player_animation"));
+					if (animation != null) {
+						animation.setAnimation(new KeyframeAnimationPlayer(PlayerAnimationRegistry.getAnimation(new ResourceLocation("power", "animation.player.detransformation"))));
+					}
+				}
+			}
+			if (!world.isClientSide()) {
+				if (entity instanceof Player && world instanceof ServerLevel srvLvl_) {
+					List<Connection> connections = srvLvl_.getServer().getConnection().getConnections();
+					synchronized (connections) {
+						Iterator<Connection> iterator = connections.iterator();
+						while (iterator.hasNext()) {
+							Connection connection = iterator.next();
+							if (!connection.isConnecting() && connection.isConnected())
+								PowerMod.PACKET_HANDLER.sendTo(new AnimationsModuleSetupProcedure.PowerModAnimationMessage(Component.literal("animation.player.detransformation"), entity.getId(), true), connection, NetworkDirection.PLAY_TO_CLIENT);
+						}
+					}
+				}
 			}
 		});
 	}
