259694a73f51168d529a98fd1f763d192163210b
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-07-14 01:06:58 +0300

    Fixed a bug that caused the mod's game rules to not work

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -3,4 +3,4 @@ What's new in Keepers of the Stones II version 1.0.0.30?<br />
 <br />- Added the ability to switch from Keepers of the Stones to Keepers of the Stones II (Some items may disappear during the transition)
 <br />- Player animations returned when activating and deactivating stones
 <br />- PlayerAnimator mod is again required for the mod to work
-<br />- [Bugfix] 
+<br />- [Bugfix] Fixed a bug that caused the mod's game rules to not work
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/EtherStoneCheckProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/EtherStoneCheckProcedure.java
@@ -6,9 +6,13 @@ import com.esmods.keepersofthestonestwo.network.PowerModVariables;
 
 public class EtherStoneCheckProcedure {
 	public static boolean execute(LevelAccessor world) {
-		if (PowerModVariables.MapVariables.get(world).ether_stone == false || PowerModVariables.MapVariables.get(world).get_limit_of_stones == false) {
-			return false;
+		if (PowerModVariables.MapVariables.get(world).get_limit_of_stones == true) {
+			if (PowerModVariables.MapVariables.get(world).ether_stone == false) {
+				return true;
+			}
+		} else if (PowerModVariables.MapVariables.get(world).get_limit_of_stones == false) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 }
