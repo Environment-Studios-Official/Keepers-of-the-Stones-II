5a1a39c94df369e8b24de04aac028bf310644ef0
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-07-30 22:51:13 +0300

    Fixes

--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
@@ -568,7 +568,6 @@ public class PowerModItems {
 	public static final RegistryObject<Item> RAW_ENERGIUM_BLOCK = block(PowerModBlocks.RAW_ENERGIUM_BLOCK);
 	public static final RegistryObject<Item> MERCURY_BATTERY = REGISTRY.register("mercury_battery", () -> new MercuryBatteryItem());
 	public static final RegistryObject<Item> MERCURY_BATTLE_AXE = REGISTRY.register("mercury_battle_axe", () -> new MercuryBattleAxeItem());
-	public static final RegistryObject<Item> CURSED_KEEPER_SPAWN_EGG = REGISTRY.register("cursed_keeper_spawn_egg", () -> new ForgeSpawnEggItem(PowerModEntities.CURSED_KEEPER, -9216650, -7256744, new Item.Properties()));
 	public static final RegistryObject<Item> DEEPSLATE_DEPLETED_ENERGIUM_ORE = block(PowerModBlocks.DEEPSLATE_DEPLETED_ENERGIUM_ORE);
 	public static final RegistryObject<Item> DEEPSLATE_ENERGIUM_ORE = block(PowerModBlocks.DEEPSLATE_ENERGIUM_ORE);
 	public static final RegistryObject<Item> DEEPSLATE_AMPLIFIER_ORE = block(PowerModBlocks.DEEPSLATE_AMPLIFIER_ORE);
