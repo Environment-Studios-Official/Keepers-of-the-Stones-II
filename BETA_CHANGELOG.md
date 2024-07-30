a8305e42b89eb9c6f7633769f49a660632bb575f
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-07-30 22:34:06 +0300

    Stopping time now stops the player completely

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,5 +1,6 @@
 What's new in Keepers of the Stones II version 1.0.0.34?<br/>
 <br />- Improved poison and rain bows, now their range depends on the time of use (like vanilla bows)
+<br />- Stopping time now stops the player completely
 <br />- Reduced battery charging time to 30 seconds instead of 5 minutes
 <br />- Added a new required mod - Pehkui
 <br />- [API] Colorful Powers API has been updated to version 11
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/TimeStoppedPriIstiechieniiEffiektaProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/TimeStoppedPriIstiechieniiEffiektaProcedure.java
@@ -1,8 +1,8 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
-import net.neoforged.neoforge.common.NeoForgeMod;
+import virtuoel.pehkui.api.ScaleTypes;
+import virtuoel.pehkui.api.ScaleOperations;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
@@ -11,8 +11,7 @@ public class TimeStoppedPriIstiechieniiEffiektaProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.MOVEMENT_SPEED).setBaseValue(0.10000000149011612);
-		((LivingEntity) entity).getAttribute(NeoForgeMod.ENTITY_GRAVITY.value()).setBaseValue(0.08);
+		ScaleTypes.MOTION.getScaleData(entity).setTargetScale((float) ScaleOperations.SET.applyAsDouble(ScaleTypes.MOTION.getScaleData(entity).getTargetScale(), 1));
 		{
 			PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
 			_vars.ability_block = false;
