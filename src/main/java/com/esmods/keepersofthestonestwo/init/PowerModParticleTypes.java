bdd775331051adc9b81759c16ffbd9fd11816173
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-01 12:50:49 +0300

    Some updates

--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModParticleTypes.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModParticleTypes.java
@@ -4,39 +4,39 @@
  */
 package com.esmods.keepersofthestonestwo.init;
 
-import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.DeferredHolder;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
 
-import net.minecraft.core.registries.Registries;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.particles.ParticleType;
 
 import com.esmods.keepersofthestonestwo.PowerMod;
 
 public class PowerModParticleTypes {
-	public static final DeferredRegister<ParticleType<?>> REGISTRY = DeferredRegister.create(Registries.PARTICLE_TYPE, PowerMod.MODID);
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BASS_BOOM = REGISTRY.register("bass_boom", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> INSECTS = REGISTRY.register("insects", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> LIGHT_SPARKLE = REGISTRY.register("light_sparkle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> VACUUM_PARTICLE = REGISTRY.register("vacuum_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> ENERGY_SPARK = REGISTRY.register("energy_spark", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> SUN_PARTICLES = REGISTRY.register("sun_particles", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> MOON_PARTICLE = REGISTRY.register("moon_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> STAR_PARTICLE = REGISTRY.register("star_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> TIME_SLOW = REGISTRY.register("time_slow", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> TIME_STOP = REGISTRY.register("time_stop", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> TIME_FAST = REGISTRY.register("time_fast", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> RUNES_OF_CREATION = REGISTRY.register("runes_of_creation", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> RUNES_OF_DESTRUCTION = REGISTRY.register("runes_of_destruction", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> TECHNOBARRIER_PARTICLE = REGISTRY.register("technobarrier_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> TELEPORTATION_PARTICLE = REGISTRY.register("teleportation_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> ENERGIUM_GOLEM_CORE_ATTACK_PARTICLE = REGISTRY.register("energium_golem_core_attack_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> AMBER_POWER = REGISTRY.register("amber_power", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BLOOD_SPLASH = REGISTRY.register("blood_splash", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> MIST_PARTICLE = REGISTRY.register("mist_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> POISON_PARTICLE = REGISTRY.register("poison_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BLUE_MAGNET_PARTICLE = REGISTRY.register("blue_magnet_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> RED_MAGNET_PARTICLE = REGISTRY.register("red_magnet_particle", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> MUSHROOM_SPORES = REGISTRY.register("mushroom_spores", () -> new SimpleParticleType(false));
-	public static final DeferredHolder<ParticleType<?>, SimpleParticleType> MERCURY_PARTICLE = REGISTRY.register("mercury_particle", () -> new SimpleParticleType(false));
+	public static final DeferredRegister<ParticleType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, PowerMod.MODID);
+	public static final RegistryObject<SimpleParticleType> BASS_BOOM = REGISTRY.register("bass_boom", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> INSECTS = REGISTRY.register("insects", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> LIGHT_SPARKLE = REGISTRY.register("light_sparkle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> VACUUM_PARTICLE = REGISTRY.register("vacuum_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> ENERGY_SPARK = REGISTRY.register("energy_spark", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> SUN_PARTICLES = REGISTRY.register("sun_particles", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> MOON_PARTICLE = REGISTRY.register("moon_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> STAR_PARTICLE = REGISTRY.register("star_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> TIME_SLOW = REGISTRY.register("time_slow", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> TIME_STOP = REGISTRY.register("time_stop", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> TIME_FAST = REGISTRY.register("time_fast", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> RUNES_OF_CREATION = REGISTRY.register("runes_of_creation", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> RUNES_OF_DESTRUCTION = REGISTRY.register("runes_of_destruction", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> TECHNOBARRIER_PARTICLE = REGISTRY.register("technobarrier_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> TELEPORTATION_PARTICLE = REGISTRY.register("teleportation_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> ENERGIUM_GOLEM_CORE_ATTACK_PARTICLE = REGISTRY.register("energium_golem_core_attack_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> AMBER_POWER = REGISTRY.register("amber_power", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> BLOOD_SPLASH = REGISTRY.register("blood_splash", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> MIST_PARTICLE = REGISTRY.register("mist_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> POISON_PARTICLE = REGISTRY.register("poison_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> BLUE_MAGNET_PARTICLE = REGISTRY.register("blue_magnet_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> RED_MAGNET_PARTICLE = REGISTRY.register("red_magnet_particle", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> MUSHROOM_SPORES = REGISTRY.register("mushroom_spores", () -> new SimpleParticleType(false));
+	public static final RegistryObject<SimpleParticleType> MERCURY_PARTICLE = REGISTRY.register("mercury_particle", () -> new SimpleParticleType(false));
 }
