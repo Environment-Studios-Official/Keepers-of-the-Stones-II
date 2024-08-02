9b312d23c75f5ab1f5916bef8479a856a9883537
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-03 01:28:40 +0300

    Fixed crush

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/AmberLayerOnEntityProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/AmberLayerOnEntityProcedure.java
@@ -5,6 +5,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
@@ -28,13 +29,15 @@ public class AmberLayerOnEntityProcedure {
 	private static void execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return;
-		if (((LivingEntity) entity).getAttribute(PowerModAttributes.SEALEDINAMBER.getDelegate()).getValue() == 1) {
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 200, 200, false, false));
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 200, 200, false, false));
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 200, 200, false, false));
+		if (!(entity instanceof ItemEntity)) {
+			if (((LivingEntity) entity).getAttribute(PowerModAttributes.SEALEDINAMBER.getDelegate()).getValue() >= 1) {
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 200, 200, false, false));
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 200, 200, false, false));
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 200, 200, false, false));
+			}
 		}
 	}
 }
