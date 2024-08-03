a4c963f50518b835cc7ed93073781fef5269da2a
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-03 13:27:25 +0300

    Fix size particle

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/WhirlwindKazhdyiTikVoVriemiaEffiektaProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/WhirlwindKazhdyiTikVoVriemiaEffiektaProcedure.java
@@ -13,7 +13,7 @@ public class WhirlwindKazhdyiTikVoVriemiaEffiektaProcedure {
 			Entity _ent = entity;
 			if (!_ent.level().isClientSide() && _ent.getServer() != null) {
 				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-						_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "particle dust{color:[0.502,0.502,0.502],scale:5} ~ ~10 ~ 2 10 2 10 100 force");
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "particle dust{color:[0.502,0.502,0.502],scale:1} ~ ~10 ~ 2 10 2 10 100 force");
 			}
 		}
 	}
