a9b86f8d6213279dd853ec3afe7cd06d237cf184
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-10 01:04:58 +0300

    Fist ablitiy for music element is done

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/MusicSpecialAttackProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/MusicSpecialAttackProcedure.java
@@ -7,27 +7,21 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.util.RandomSource;
-import net.minecraft.util.Mth;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
 
 import java.util.List;
 import java.util.Comparator;
 
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
-import com.esmods.keepersofthestonestwo.init.PowerModParticleTypes;
 import com.esmods.keepersofthestonestwo.init.PowerModEntities;
 import com.esmods.keepersofthestonestwo.entity.MercuryBallProjectileEntity;
 
@@ -49,7 +43,7 @@ public class MusicSpecialAttackProcedure {
 		double playerPosY = 0;
 		double playerPosZ = 0;
 		double playerPosX = 0;
-		if ((entity.getData(PowerModVariables.PLAYER_VARIABLES).attack).equals("mercury_attack_1")) {
+		if ((entity.getData(PowerModVariables.PLAYER_VARIABLES).attack).equals("music_attack_1")) {
 			if (entity.getData(PowerModVariables.PLAYER_VARIABLES).power >= 15) {
 				for (int index0 = 0; index0 < 15; index0++) {
 					if (!world.getBlockState(new BlockPos(
@@ -62,7 +56,7 @@ public class MusicSpecialAttackProcedure {
 						break;
 					}
 					if (world instanceof ServerLevel _level)
-						_level.sendParticles((SimpleParticleType) (PowerModParticleTypes.MERCURY_PARTICLE.get()),
+						_level.sendParticles(ParticleTypes.NOTE,
 								(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos().getX()),
 								(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos().getY()),
 								(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos().getZ()),
@@ -76,21 +70,17 @@ public class MusicSpecialAttackProcedure {
 						List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(1.1 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 						for (Entity entityiterator : _entfound) {
 							if (!(entityiterator == entity)) {
-								if (entityiterator instanceof LivingEntity _entity && !_entity.level().isClientSide())
-									_entity.addEffect(new MobEffectInstance(MobEffects.HUNGER, 120, 3, false, false));
-								if (entityiterator instanceof LivingEntity _entity && !_entity.level().isClientSide())
-									_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 60, 2, false, false));
 								entityiterator.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("power:elemental_powers"))), entity),
-										(float) 10.13);
+										(float) 13.5);
 							}
 						}
 					}
 				}
 				if (world instanceof Level _level) {
 					if (!_level.isClientSide()) {
-						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.bubble_column.bubble_pop")), SoundSource.PLAYERS, 1, 1);
+						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.note_block.guitar")), SoundSource.PLAYERS, 1, 1);
 					} else {
-						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.bubble_column.bubble_pop")), SoundSource.PLAYERS, 1, 1, false);
+						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.note_block.guitar")), SoundSource.PLAYERS, 1, 1, false);
 					}
 				}
 				{
@@ -99,7 +89,7 @@ public class MusicSpecialAttackProcedure {
 					_vars.syncPlayerVariables(entity);
 				}
 			}
-		} else if ((entity.getData(PowerModVariables.PLAYER_VARIABLES).attack).equals("mercury_attack_2")) {
+		} else if ((entity.getData(PowerModVariables.PLAYER_VARIABLES).attack).equals("music_attack_2")) {
 			if (entity.getData(PowerModVariables.PLAYER_VARIABLES).power >= 35) {
 				{
 					Entity _shootFrom = entity;
@@ -133,176 +123,12 @@ public class MusicSpecialAttackProcedure {
 					_vars.syncPlayerVariables(entity);
 				}
 			}
-		} else if ((entity.getData(PowerModVariables.PLAYER_VARIABLES).attack).equals("mercury_attack_3")) {
+		} else if ((entity.getData(PowerModVariables.PLAYER_VARIABLES).attack).equals("music_attack_3")) {
 			if (entity.getData(PowerModVariables.PLAYER_VARIABLES).power >= 55) {
-				if (world.getBiome(BlockPos.containing(x, y, z)).value().getBaseTemperature() * 100f <= 0.15) {
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.anvil.hit")), SoundSource.PLAYERS, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.anvil.hit")), SoundSource.PLAYERS, 1, 1, false);
-						}
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot((-1), 0, (-1), (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot(1, 0, 1, (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot((-1), 0, 1, (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot(1, 0, (-1), (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot(0, 0, (-1), (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot(0, 0, 1, (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot((-1), 0, 0, (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					if (world instanceof ServerLevel projectileLevel) {
-						Projectile _entityToSpawn = new Object() {
-							public Projectile getArrow(Level level, Entity shooter, float damage, int knockback) {
-								AbstractArrow entityToSpawn = new MercuryBallProjectileEntity(PowerModEntities.MERCURY_BALL_PROJECTILE.get(), level);
-								entityToSpawn.setOwner(shooter);
-								entityToSpawn.setBaseDamage(damage);
-								entityToSpawn.setKnockback(knockback);
-								entityToSpawn.setSilent(true);
-								return entityToSpawn;
-							}
-						}.getArrow(projectileLevel, entity, (float) 24.3, 0);
-						_entityToSpawn.setPos(x, (y + entity.getBbHeight() / 1.5), z);
-						_entityToSpawn.shoot(1, 0, 0, (float) 1.5, 0);
-						projectileLevel.addFreshEntity(_entityToSpawn);
-					}
-					{
-						PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
-						_vars.power = entity.getData(PowerModVariables.PLAYER_VARIABLES).power - 55;
-						_vars.syncPlayerVariables(entity);
-					}
-				} else {
-					particleAmount = 30;
-					particleRadius = 5;
-					for (int index1 = 0; index1 < (int) particleAmount; index1++) {
-						if (world instanceof ServerLevel _level)
-							_level.sendParticles((SimpleParticleType) (PowerModParticleTypes.MERCURY_PARTICLE.get()), (x + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius),
-									(y + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (z + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), 1, (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)),
-									(Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), 0.25);
-					}
-					{
-						final Vec3 _center = new Vec3(x, y, z);
-						List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(5 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
-						for (Entity entityiterator : _entfound) {
-							if (!(entityiterator == entity)) {
-								entityiterator.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("power:elemental_powers"))), entity),
-										(float) 24.3);
-								if (entityiterator instanceof LivingEntity _entity && !_entity.level().isClientSide())
-									_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 400, 4, false, false));
-								if (entityiterator instanceof LivingEntity _entity && !_entity.level().isClientSide())
-									_entity.addEffect(new MobEffectInstance(MobEffects.HUNGER, 600, 3, false, false));
-							}
-						}
-					}
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.bubble_column.bubble_pop")), SoundSource.PLAYERS, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("block.bubble_column.bubble_pop")), SoundSource.PLAYERS, 1, 1, false);
-						}
-					}
-					{
-						PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
-						_vars.power = entity.getData(PowerModVariables.PLAYER_VARIABLES).power - 55;
-						_vars.syncPlayerVariables(entity);
-					}
+				{
+					PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+					_vars.power = entity.getData(PowerModVariables.PLAYER_VARIABLES).power - 55;
+					_vars.syncPlayerVariables(entity);
 				}
 			}
 		}
