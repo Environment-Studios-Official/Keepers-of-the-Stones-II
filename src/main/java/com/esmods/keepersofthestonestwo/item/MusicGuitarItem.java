6dfc35ad26ad13173550bbb07f0fec6693b26f92
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-17 15:34:14 +0300

    Fixes

--- src/main/java/com/esmods/keepersofthestonestwo/item/MusicGuitarItem.java
+++ src/main/java/com/esmods/keepersofthestonestwo/item/MusicGuitarItem.java
@@ -12,7 +12,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.Entity;
 
-import com.esmods.keepersofthestonestwo.procedures.ExplosionMaceKazhdyiTikVInvientarieProcedure;
+import com.esmods.keepersofthestonestwo.procedures.MusicGuitarKazhdyiTikVInvientarieProcedure;
 
 public class MusicGuitarItem extends SwordItem {
 	public MusicGuitarItem() {
@@ -46,7 +46,7 @@ public class MusicGuitarItem extends SwordItem {
 	@Override
 	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
 		super.inventoryTick(itemstack, world, entity, slot, selected);
-		ExplosionMaceKazhdyiTikVInvientarieProcedure.execute(entity);
+		MusicGuitarKazhdyiTikVInvientarieProcedure.execute(entity);
 	}
 
 	@Override
