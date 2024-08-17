c45b082dba944c0589f5dcae06b4ac710683afab
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-17 18:34:37 +0300

    Added icons for music element

--- src/main/java/com/esmods/keepersofthestonestwo/client/gui/WheelAbilitiesMusicScreen.java
+++ src/main/java/com/esmods/keepersofthestonestwo/client/gui/WheelAbilitiesMusicScreen.java
@@ -36,9 +36,9 @@ public class WheelAbilitiesMusicScreen extends AbstractContainerScreen<WheelAbil
 	ImageButton imagebutton_fake_wheel_button_1;
 	ImageButton imagebutton_fake_wheel_button_2;
 	ImageButton imagebutton_fake_wheel_button_3;
-	ImageButton imagebutton_ability_1;
-	ImageButton imagebutton_ability_2;
-	ImageButton imagebutton_ability_3;
+	ImageButton imagebutton_musical_attack;
+	ImageButton imagebutton_explosion_of_notes;
+	ImageButton imagebutton_musical_relaxation;
 
 	public WheelAbilitiesMusicScreen(WheelAbilitiesMusicMenu container, Inventory inventory, Component text) {
 		super(container, inventory, text);
@@ -56,6 +56,12 @@ public class WheelAbilitiesMusicScreen extends AbstractContainerScreen<WheelAbil
 		this.renderBackground(guiGraphics, mouseX, mouseY, partialTicks);
 		super.render(guiGraphics, mouseX, mouseY, partialTicks);
 		this.renderTooltip(guiGraphics, mouseX, mouseY);
+		if (mouseX > leftPos + 86 && mouseX < leftPos + 110 && mouseY > topPos + 23 && mouseY < topPos + 47)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_music.tooltip_musical_attack_uses_15"), mouseX, mouseY);
+		if (mouseX > leftPos + 144 && mouseX < leftPos + 168 && mouseY > topPos + 84 && mouseY < topPos + 108)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_music.tooltip_explosion_of_notes_uses_40"), mouseX, mouseY);
+		if (mouseX > leftPos + 82 && mouseX < leftPos + 106 && mouseY > topPos + 146 && mouseY < topPos + 170)
+			guiGraphics.renderTooltip(font, Component.translatable("gui.power.wheel_abilities_music.tooltip_musical_relaxation_uses_75"), mouseX, mouseY);
 	}
 
 	@Override
@@ -175,8 +181,8 @@ public class WheelAbilitiesMusicScreen extends AbstractContainerScreen<WheelAbil
 		};
 		guistate.put("button:imagebutton_fake_wheel_button_3", imagebutton_fake_wheel_button_3);
 		this.addRenderableWidget(imagebutton_fake_wheel_button_3);
-		imagebutton_ability_1 = new ImageButton(this.leftPos + 72, this.topPos + 12, 46, 46, new WidgetSprites(new ResourceLocation("power:textures/screens/ability_1.png"), new ResourceLocation("power:textures/screens/ability_1_highlight.png")),
-				e -> {
+		imagebutton_musical_attack = new ImageButton(this.leftPos + 72, this.topPos + 12, 46, 46,
+				new WidgetSprites(new ResourceLocation("power:textures/screens/musical_attack.png"), new ResourceLocation("power:textures/screens/musical_attack_highlight.png")), e -> {
 					if (true) {
 						PacketDistributor.SERVER.noArg().send(new WheelAbilitiesMusicButtonMessage(6, x, y, z));
 						WheelAbilitiesMusicButtonMessage.handleButtonAction(entity, 6, x, y, z);
@@ -187,10 +193,10 @@ public class WheelAbilitiesMusicScreen extends AbstractContainerScreen<WheelAbil
 				guiGraphics.blit(sprites.get(isActive(), isHoveredOrFocused()), getX(), getY(), 0, 0, width, height, width, height);
 			}
 		};
-		guistate.put("button:imagebutton_ability_1", imagebutton_ability_1);
-		this.addRenderableWidget(imagebutton_ability_1);
-		imagebutton_ability_2 = new ImageButton(this.leftPos + 133, this.topPos + 73, 46, 46, new WidgetSprites(new ResourceLocation("power:textures/screens/ability_2.png"), new ResourceLocation("power:textures/screens/ability_2_highlight.png")),
-				e -> {
+		guistate.put("button:imagebutton_musical_attack", imagebutton_musical_attack);
+		this.addRenderableWidget(imagebutton_musical_attack);
+		imagebutton_explosion_of_notes = new ImageButton(this.leftPos + 133, this.topPos + 73, 46, 46,
+				new WidgetSprites(new ResourceLocation("power:textures/screens/explosion_of_notes.png"), new ResourceLocation("power:textures/screens/explosion_of_notes_highlight.png")), e -> {
 					if (true) {
 						PacketDistributor.SERVER.noArg().send(new WheelAbilitiesMusicButtonMessage(7, x, y, z));
 						WheelAbilitiesMusicButtonMessage.handleButtonAction(entity, 7, x, y, z);
@@ -201,10 +207,10 @@ public class WheelAbilitiesMusicScreen extends AbstractContainerScreen<WheelAbil
 				guiGraphics.blit(sprites.get(isActive(), isHoveredOrFocused()), getX(), getY(), 0, 0, width, height, width, height);
 			}
 		};
-		guistate.put("button:imagebutton_ability_2", imagebutton_ability_2);
-		this.addRenderableWidget(imagebutton_ability_2);
-		imagebutton_ability_3 = new ImageButton(this.leftPos + 72, this.topPos + 134, 46, 46, new WidgetSprites(new ResourceLocation("power:textures/screens/ability_3.png"), new ResourceLocation("power:textures/screens/ability_3_highlight.png")),
-				e -> {
+		guistate.put("button:imagebutton_explosion_of_notes", imagebutton_explosion_of_notes);
+		this.addRenderableWidget(imagebutton_explosion_of_notes);
+		imagebutton_musical_relaxation = new ImageButton(this.leftPos + 72, this.topPos + 134, 46, 46,
+				new WidgetSprites(new ResourceLocation("power:textures/screens/musical_relaxation.png"), new ResourceLocation("power:textures/screens/musical_relaxation_highlight.png")), e -> {
 					if (true) {
 						PacketDistributor.SERVER.noArg().send(new WheelAbilitiesMusicButtonMessage(8, x, y, z));
 						WheelAbilitiesMusicButtonMessage.handleButtonAction(entity, 8, x, y, z);
@@ -215,7 +221,7 @@ public class WheelAbilitiesMusicScreen extends AbstractContainerScreen<WheelAbil
 				guiGraphics.blit(sprites.get(isActive(), isHoveredOrFocused()), getX(), getY(), 0, 0, width, height, width, height);
 			}
 		};
-		guistate.put("button:imagebutton_ability_3", imagebutton_ability_3);
-		this.addRenderableWidget(imagebutton_ability_3);
+		guistate.put("button:imagebutton_musical_relaxation", imagebutton_musical_relaxation);
+		this.addRenderableWidget(imagebutton_musical_relaxation);
 	}
 }
