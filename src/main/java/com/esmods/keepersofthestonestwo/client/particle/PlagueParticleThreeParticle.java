27e37300a1ea177a627d8db0f9cf5beb881dc64d
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-18 15:35:51 +0300

    Added plague particles

---
+++ src/main/java/com/esmods/keepersofthestonestwo/client/particle/PlagueParticleThreeParticle.java
@@ -0,0 +1,57 @@
+
+package com.esmods.keepersofthestonestwo.client.particle;
+
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.api.distmarker.Dist;
+
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+@OnlyIn(Dist.CLIENT)
+public class PlagueParticleThreeParticle extends TextureSheetParticle {
+	public static PlagueParticleThreeParticleProvider provider(SpriteSet spriteSet) {
+		return new PlagueParticleThreeParticleProvider(spriteSet);
+	}
+
+	public static class PlagueParticleThreeParticleProvider implements ParticleProvider<SimpleParticleType> {
+		private final SpriteSet spriteSet;
+
+		public PlagueParticleThreeParticleProvider(SpriteSet spriteSet) {
+			this.spriteSet = spriteSet;
+		}
+
+		public Particle createParticle(SimpleParticleType typeIn, ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+			return new PlagueParticleThreeParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+		}
+	}
+
+	private final SpriteSet spriteSet;
+
+	protected PlagueParticleThreeParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet) {
+		super(world, x, y, z);
+		this.spriteSet = spriteSet;
+		this.setSize(1f, 1f);
+		this.lifetime = 20;
+		this.gravity = 0f;
+		this.hasPhysics = true;
+		this.xd = vx * 0.1;
+		this.yd = vy * 0.1;
+		this.zd = vz * 0.1;
+		this.pickSprite(spriteSet);
+	}
+
+	@Override
+	public ParticleRenderType getRenderType() {
+		return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+	}
+}
