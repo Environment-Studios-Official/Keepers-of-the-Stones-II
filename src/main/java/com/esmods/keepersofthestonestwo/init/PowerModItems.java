4394b1e0e85f6230801870cc1f051c99e37645ad
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-08-31 23:56:29 +0300

    Added base for smoke element, part 1

--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModItems.java
@@ -62,6 +62,7 @@ import com.esmods.keepersofthestonestwo.item.SoundBatteryItem;
 import com.esmods.keepersofthestonestwo.item.SoundArmorItem;
 import com.esmods.keepersofthestonestwo.item.SolarBladeItem;
 import com.esmods.keepersofthestonestwo.item.SmokeStoneItem;
+import com.esmods.keepersofthestonestwo.item.SmokeArmorItem;
 import com.esmods.keepersofthestonestwo.item.ShadowStoneItem;
 import com.esmods.keepersofthestonestwo.item.ShadowScytheItem;
 import com.esmods.keepersofthestonestwo.item.ShadowBatteryItem;
@@ -623,6 +624,10 @@ public class PowerModItems {
 	public static final DeferredHolder<Item, Item> GRAVITY_ARMOR_BOOTS = REGISTRY.register("gravity_armor_boots", () -> new GravityArmorItem.Boots());
 	public static final DeferredHolder<Item, Item> GRAVITY_BATTERY = REGISTRY.register("gravity_battery", () -> new GravityBatteryItem());
 	public static final DeferredHolder<Item, Item> GRAVITY_MACE = REGISTRY.register("gravity_mace", () -> new GravityMaceItem());
+	public static final DeferredHolder<Item, Item> SMOKE_ARMOR_HELMET = REGISTRY.register("smoke_armor_helmet", () -> new SmokeArmorItem.Helmet());
+	public static final DeferredHolder<Item, Item> SMOKE_ARMOR_CHESTPLATE = REGISTRY.register("smoke_armor_chestplate", () -> new SmokeArmorItem.Chestplate());
+	public static final DeferredHolder<Item, Item> SMOKE_ARMOR_LEGGINGS = REGISTRY.register("smoke_armor_leggings", () -> new SmokeArmorItem.Leggings());
+	public static final DeferredHolder<Item, Item> SMOKE_ARMOR_BOOTS = REGISTRY.register("smoke_armor_boots", () -> new SmokeArmorItem.Boots());
 
 	// Start of user code block custom items
 	// End of user code block custom items
--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModTabs.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModTabs.java
@@ -136,6 +136,7 @@ public class PowerModTabs {
 				tabData.accept(PowerModItems.PLAGUE_STONE.get());
 				tabData.accept(PowerModItems.BLUE_FLAME_STONE.get());
 				tabData.accept(PowerModItems.GRAVITY_STONE.get());
+				tabData.accept(PowerModItems.SMOKE_STONE.get());
 			})
 
 					.build());
