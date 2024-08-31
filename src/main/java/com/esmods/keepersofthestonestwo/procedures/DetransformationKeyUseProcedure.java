46e0100211a13f9a713ef15afd2d81157eb65fc8
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-01 00:02:59 +0300

    Added base for smoke element, part 2 (FIX)

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/DetransformationKeyUseProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/DetransformationKeyUseProcedure.java
@@ -110,6 +110,8 @@ public class DetransformationKeyUseProcedure {
 					_entity.removeEffect(PowerModMobEffects.BLUE_FLAME_MASTER.get());
 				if (entity instanceof LivingEntity _entity)
 					_entity.removeEffect(PowerModMobEffects.GRAVITY_MASTER.get());
+				if (entity instanceof LivingEntity _entity)
+					_entity.removeEffect(PowerModMobEffects.SMOKE_MASTER.get());
 				{
 					PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
 					_vars.active = false;
