35b1f7536c3874f246954c907bba40185b149a29
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-01 23:05:24 +0300

    Fixed a bug that caused the transformation animation to not play (Only 1.20.6)

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,4 +1,5 @@
 What's new in Keepers of the Stones II version 1.0.0.40?<br/>
 <br />- Abilities in the ability wheel are now locked when the Power Lock effect is active
 <br />- [API] Colorful Powers API has been updated to version 15
+<br />- [Bugfix] Fixed a bug that caused the transformation animation to not play (Only 1.20.6)
 <br />- [Bugfix] Fixed a bug that caused vacuum battery not to work
--- src/main/java/com/esmods/keepersofthestonestwo/network/PowerModVariables.java
+++ src/main/java/com/esmods/keepersofthestonestwo/network/PowerModVariables.java
@@ -110,6 +110,7 @@ public class PowerModVariables {
 				clone.fake_element_name_second_timer = original.fake_element_name_second_timer;
 				clone.fake_element_name_third_timer = original.fake_element_name_third_timer;
 				clone.send_client_package = original.send_client_package;
+				clone.detransform_anim_trigger = original.detransform_anim_trigger;
 			}
 			event.getEntity().setData(PLAYER_VARIABLES, clone);
 		}
@@ -457,6 +458,7 @@ public class PowerModVariables {
 		public String first_booster_slot = "0";
 		public String second_booster_slot = "0";
 		public String third_booster_slot = "0";
+		public boolean detransform_anim_trigger = false;
 
 		@Override
 		public CompoundTag serializeNBT(HolderLookup.Provider lookupProvider) {
@@ -502,6 +504,7 @@ public class PowerModVariables {
 			nbt.putString("first_booster_slot", first_booster_slot);
 			nbt.putString("second_booster_slot", second_booster_slot);
 			nbt.putString("third_booster_slot", third_booster_slot);
+			nbt.putBoolean("detransform_anim_trigger", detransform_anim_trigger);
 			return nbt;
 		}
 
@@ -548,6 +551,7 @@ public class PowerModVariables {
 			first_booster_slot = nbt.getString("first_booster_slot");
 			second_booster_slot = nbt.getString("second_booster_slot");
 			third_booster_slot = nbt.getString("third_booster_slot");
+			detransform_anim_trigger = nbt.getBoolean("detransform_anim_trigger");
 		}
 
 		public void syncPlayerVariables(Entity entity) {
