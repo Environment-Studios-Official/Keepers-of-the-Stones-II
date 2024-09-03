1ecee35bfda774c5ce5b2677cb07abe37c072858
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-03 21:04:40 +0300

    Fog render fixes

---
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/RenderItemOnBatteryChargerProcedure.java
@@ -0,0 +1,9 @@
+package com.esmods.keepersofthestonestwo.procedures;
+
+public class RenderItemOnBatteryChargerProcedure {
+	public static void execute() {
+		double xpos = 0;
+		double ypos = 0;
+		double zpos = 0;
+	}
+}
