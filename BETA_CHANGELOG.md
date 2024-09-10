f623576f6e0a45a40cc144da8f8d0c26ca0e6055
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-11 00:13:13 +0300

    The ball lightning attack now creates regular lightning bolts

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,4 +1,5 @@
 What's new in Keepers of the Stones II version 1.0.0.42<br/>
 <br />- Increased the damage of rain and poison bows to 12
+<br />- The ball lightning attack now creates regular lightning bolts
 <br />- [Bugfix] Fixed a bug that caused amplifier and copyright deepslate ores to be generated in the world
 <br />- [Bugfix] Fixed a bug that caused the Cursed Forest to be generated in Nether
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/BallLightningProjectileKoghdaSnariadPopadaietVBlokProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/BallLightningProjectileKoghdaSnariadPopadaietVBlokProcedure.java
@@ -1,10 +1,20 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.BlockPos;
 
 public class BallLightningProjectileKoghdaSnariadPopadaietVBlokProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
+		if (world instanceof ServerLevel _level) {
+			LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
+			entityToSpawn.moveTo(Vec3.atBottomCenterOf(BlockPos.containing(x, y, z)));;
+			_level.addFreshEntity(entityToSpawn);
+		}
 		if (world instanceof Level _level && !_level.isClientSide())
 			_level.explode(null, x, y, z, 9, Level.ExplosionInteraction.MOB);
 	}
