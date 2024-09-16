f8d97958c605be09551502dcfae916fb7bd60e62
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-16 13:27:36 +0300

    Spawn fixes

--- src/main/java/com/esmods/keepersofthestonestwo/entity/CursedKnightEntity.java
+++ src/main/java/com/esmods/keepersofthestonestwo/entity/CursedKnightEntity.java
@@ -31,7 +31,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.Difficulty;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -157,7 +157,7 @@ public class CursedKnightEntity extends Monster implements GeoEntity {
 
 	public static void init() {
 		SpawnPlacements.register(PowerModEntities.CURSED_KNIGHT.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-				(entityType, world, reason, pos, random) -> (world.getDifficulty() != Difficulty.PEACEFUL && Monster.isDarkEnoughToSpawn(world, pos, random) && Mob.checkMobSpawnRules(entityType, world, reason, pos, random)));
+				(entityType, world, reason, pos, random) -> (world.getBlockState(pos.below()).is(BlockTags.ANIMALS_SPAWNABLE_ON) && world.getRawBrightness(pos, 0) > 8));
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
