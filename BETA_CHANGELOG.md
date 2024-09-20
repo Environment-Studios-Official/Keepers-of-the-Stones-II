0e65ebf83cd75638233a46fc3674ad41a1d9da17
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-20 23:22:25 +0300

    Fixed a bug where water could spill using the ability

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -3,5 +3,6 @@ What's new in Keepers of the Stones II version 1.0.0.43<br/>
 <br />- [API] Colorful Powers API has been updated to version 16
 <br />- [Bugfix] Fixed damage from the Smoke Master's musket
 <br />- [Bugfix] Fixed the sound of the musket
+<br />- [Bugfix] Fixed a bug where water could spill using the ability
 <br />- [Bugfix] Fixed a bug that caused the lava surface to break bedrock
 <br />- [Bugfix] Fixed a bug where it was possible to activate more than 3 stones
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/WaterAttackKoghdaSnariadPopadaietVBlokProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/WaterAttackKoghdaSnariadPopadaietVBlokProcedure.java
@@ -2,15 +2,19 @@ package com.esmods.keepersofthestonestwo.procedures;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
 
 public class WaterAttackKoghdaSnariadPopadaietVBlokProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
 		if (world instanceof ServerLevel _level)
 			_level.sendParticles(ParticleTypes.SPLASH, x, y, z, 25, 1, 1, 1, 1);
-		if ((world.getBlockState(BlockPos.containing(x, y + 1, z))).getBlock() == Blocks.AIR) {
+		if ((world.getBlockState(BlockPos.containing(x, y + 1, z))).getBlock() == Blocks.AIR && !((entity.level().dimension()) == Level.NETHER)) {
 			world.setBlock(BlockPos.containing(x, y + 1, z), Blocks.WATER.defaultBlockState(), 3);
 		}
 	}
