67d4f715e54f2d9f740190fa41d340d9fc87a24c
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-22 17:58:34 +0300

    Energium and cursed vaults now display the most valuable item you can get

---
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/RenderItemInEnergiumVaultsProcedure.java
@@ -0,0 +1,274 @@
+package com.esmods.keepersofthestonestwo.procedures;
+
+import org.joml.Matrix4f;
+
+import net.neoforged.neoforge.client.model.data.ModelData;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+import net.neoforged.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.core.SectionPos;
+import net.minecraft.core.BlockPos;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.block.ModelBlockRenderer;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.Minecraft;
+
+import javax.annotation.Nullable;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import com.esmods.keepersofthestonestwo.network.PowerModVariables;
+import com.esmods.keepersofthestonestwo.init.PowerModItems;
+import com.esmods.keepersofthestonestwo.init.PowerModBlocks;
+
+@EventBusSubscriber(value = Dist.CLIENT)
+public class RenderItemInEnergiumVaultsProcedure {
+	private static RenderLevelStageEvent provider = null;
+	private static Map<EntityType, Entity> data = new HashMap<>();
+	private static float textWidth = 1.0F;
+	private static float textHeight = 1.0F;
+	private static int textColor = -1;
+	private static int backColor = 0;
+
+	public static void setBackColor(int color) {
+		RenderItemInEnergiumVaultsProcedure.backColor = color;
+	}
+
+	public static void setTextColor(int color) {
+		RenderItemInEnergiumVaultsProcedure.textColor = color;
+	}
+
+	public static void setScale(float width, float height) {
+		RenderItemInEnergiumVaultsProcedure.textWidth = width;
+		RenderItemInEnergiumVaultsProcedure.textHeight = height;
+	}
+
+	public static void renderBlock(BlockState blockState, double x, double y, double z, float yaw, float pitch, float roll, float scale, boolean glowing) {
+		BlockPos blockPos = BlockPos.containing(x, y, z);
+		Vec3 pos = provider.getCamera().getPosition();
+		int packedLight = glowing ? LightTexture.FULL_BRIGHT : LevelRenderer.getLightColor(Minecraft.getInstance().level, blockPos);
+		PoseStack poseStack = provider.getPoseStack();
+		poseStack.pushPose();
+		poseStack.translate(x - pos.x(), y - pos.y(), z - pos.z());
+		poseStack.mulPose(com.mojang.math.Axis.YN.rotationDegrees(yaw));
+		poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(pitch));
+		poseStack.mulPose(com.mojang.math.Axis.ZN.rotationDegrees(roll));
+		poseStack.scale(scale, scale, scale);
+		poseStack.translate(-0.5F, -0.5F, -0.5F);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		renderBlockModel(blockState, blockPos, poseStack, packedLight);
+		renderBlockEntity(blockState, blockPos, poseStack, packedLight);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		poseStack.popPose();
+	}
+
+	private static void renderBlockEntity(BlockState blockState, BlockPos blockPos, PoseStack poseStack, int packedLight) {
+		if (blockState.getBlock() instanceof EntityBlock entityBlock) {
+			Minecraft minecraft = Minecraft.getInstance();
+			ClientLevel level = minecraft.level;
+			BlockEntity blockEntity = entityBlock.newBlockEntity(blockPos, blockState);
+			if (blockEntity != null) {
+				BlockEntityRenderer blockEntityRenderer = minecraft.getBlockEntityRenderDispatcher().getRenderer(blockEntity);
+				if (blockEntityRenderer != null) {
+					MultiBufferSource.BufferSource bufferSource = minecraft.renderBuffers().bufferSource();
+					blockEntity.setLevel(level);
+					blockEntityRenderer.render(blockEntity, 0.0F, poseStack, bufferSource, packedLight, OverlayTexture.NO_OVERLAY);
+				}
+			}
+		}
+	}
+
+	private static void renderBlockModel(BlockState blockState, BlockPos blockPos, PoseStack poseStack, int packedLight) {
+		if (blockState.getRenderShape() == RenderShape.MODEL) {
+			Minecraft minecraft = Minecraft.getInstance();
+			ClientLevel level = minecraft.level;
+			MultiBufferSource.BufferSource bufferSource = minecraft.renderBuffers().bufferSource();
+			BlockRenderDispatcher dispatcher = minecraft.getBlockRenderer();
+			ModelBlockRenderer renderer = dispatcher.getModelRenderer();
+			BakedModel bakedModel = dispatcher.getBlockModel(blockState);
+			ModelData modelData = bakedModel.getModelData(level, blockPos, blockState, ModelData.builder().build());
+			PoseStack.Pose pose = poseStack.last();
+			int color = minecraft.getBlockColors().getColor(blockState, level, blockPos);
+			float red = (color >> 16 & 255) / 255.0F;
+			float green = (color >> 8 & 255) / 255.0F;
+			float blue = (color & 255) / 255.0F;
+			for (RenderType renderType : bakedModel.getRenderTypes(blockState, RandomSource.create(42L), modelData)) {
+				renderer.renderModel(pose, bufferSource.getBuffer(Sheets.translucentCullBlockSheet()), blockState, bakedModel, red, green, blue, packedLight, OverlayTexture.NO_OVERLAY, modelData, renderType);
+			}
+		}
+	}
+
+	public static void renderEntity(EntityType type, double x, double y, double z, float yaw, float pitch, float roll, float scale, boolean glowing) {
+		if (type == null)
+			return;
+		Entity entity;
+		ClientLevel level = Minecraft.getInstance().level;
+		if (data.containsKey(type)) {
+			entity = data.get(type);
+			if (entity.level() != level) {
+				entity = type.create(level);
+				data.put(type, entity);
+			}
+		} else {
+			entity = type.create(level);
+			data.put(type, entity);
+		}
+		renderEntity(entity, 0.0F, x, y, z, yaw, pitch, roll, scale, glowing ? LightTexture.FULL_BRIGHT : LevelRenderer.getLightColor(level, BlockPos.containing(x, y, z)));
+	}
+
+	public static void renderEntity(Entity entity, double x, double y, double z, float yaw, float pitch, float roll, float scale, boolean glowing) {
+		float partialTick = provider.getPartialTick().getGameTimeDeltaTicks();
+		int packedLight = glowing ? LightTexture.FULL_BRIGHT : Minecraft.getInstance().getEntityRenderDispatcher().getPackedLightCoords(entity, partialTick);
+		renderEntity(entity, partialTick, x, y, z, yaw, pitch, roll, scale, packedLight);
+	}
+
+	private static void renderEntity(Entity entity, float partialTick, double x, double y, double z, float yaw, float pitch, float roll, float scale, int packedLight) {
+		if (entity == null)
+			return;
+		Minecraft minecraft = Minecraft.getInstance();
+		MultiBufferSource.BufferSource bufferSource = minecraft.renderBuffers().bufferSource();
+		EntityRenderer renderer = minecraft.getEntityRenderDispatcher().getRenderer(entity);
+		Vec3 pos = provider.getCamera().getPosition();
+		float offset = (entity.getBbHeight() / 2.0F) * scale;
+		PoseStack poseStack = provider.getPoseStack();
+		poseStack.pushPose();
+		poseStack.translate(x - pos.x(), y + offset - pos.y(), z - pos.z());
+		poseStack.mulPose(com.mojang.math.Axis.YN.rotationDegrees(yaw));
+		poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(pitch));
+		poseStack.mulPose(com.mojang.math.Axis.ZN.rotationDegrees(roll));
+		poseStack.translate(0.0F, -offset, 0.0F);
+		poseStack.scale(scale, scale, scale);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		renderer.render(entity, entity.getViewYRot(partialTick), partialTick, poseStack, bufferSource, packedLight);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		poseStack.popPose();
+	}
+
+	public static void renderItem(ItemStack itemStack, double x, double y, double z, float yaw, float pitch, float roll, float scale, boolean flipping, boolean glowing) {
+		Minecraft minecraft = Minecraft.getInstance();
+		MultiBufferSource.BufferSource bufferSource = minecraft.renderBuffers().bufferSource();
+		ItemRenderer renderer = minecraft.getItemRenderer();
+		Vec3 pos = provider.getCamera().getPosition();
+		int packedLight = glowing ? LightTexture.FULL_BRIGHT : LevelRenderer.getLightColor(minecraft.level, BlockPos.containing(x, y, z));
+		PoseStack poseStack = provider.getPoseStack();
+		poseStack.pushPose();
+		poseStack.translate(x - pos.x(), y - pos.y(), z - pos.z());
+		poseStack.mulPose(com.mojang.math.Axis.YN.rotationDegrees(yaw));
+		poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(pitch));
+		poseStack.mulPose(com.mojang.math.Axis.ZN.rotationDegrees(roll));
+		poseStack.scale(scale, scale, scale);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		renderer.renderStatic(null, itemStack, ItemDisplayContext.FIXED, flipping, poseStack, bufferSource, minecraft.level, packedLight, OverlayTexture.NO_OVERLAY, 0);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		poseStack.popPose();
+	}
+
+	public static void renderTexts(String texts, double x, double y, double z, float yaw, float pitch, float roll, boolean glowing) {
+		Minecraft minecraft = Minecraft.getInstance();
+		MultiBufferSource.BufferSource bufferSource = minecraft.renderBuffers().bufferSource();
+		Font font = minecraft.font;
+		Vec3 pos = provider.getCamera().getPosition();
+		int packedLight = glowing ? LightTexture.FULL_BRIGHT : LevelRenderer.getLightColor(minecraft.level, BlockPos.containing(x, y, z));
+		PoseStack poseStack = provider.getPoseStack();
+		poseStack.pushPose();
+		poseStack.translate(x - pos.x(), y - pos.y(), z - pos.z());
+		poseStack.mulPose(com.mojang.math.Axis.YN.rotationDegrees(yaw));
+		poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(pitch));
+		poseStack.mulPose(com.mojang.math.Axis.ZN.rotationDegrees(roll));
+		poseStack.scale(textWidth, -textHeight, 1.0F);
+		poseStack.translate((font.width(texts) - 1) * -0.5F, (font.lineHeight - 1) * -0.5F, 0.0F);
+		Matrix4f matrix4f = poseStack.last().pose();
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		if (backColor != 0)
+			font.drawInBatch(texts, 0.0F, 0.0F, 0, false, matrix4f, bufferSource, Font.DisplayMode.SEE_THROUGH, backColor, packedLight);
+		font.drawInBatch(texts, 0.0F, 0.0F, textColor, false, matrix4f, bufferSource, Font.DisplayMode.NORMAL, 0, packedLight);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		poseStack.popPose();
+	}
+
+	@SubscribeEvent
+	public static void renderModels(RenderLevelStageEvent event) {
+		provider = event;
+		if (provider.getStage() == RenderLevelStageEvent.Stage.AFTER_ENTITIES) {
+			ClientLevel level = Minecraft.getInstance().level;
+			Entity entity = provider.getCamera().getEntity();
+			Vec3 pos = entity.getPosition(provider.getPartialTick().getGameTimeDeltaTicks());
+			RenderSystem.depthMask(true);
+			RenderSystem.enableDepthTest();
+			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+			execute(provider, level);
+			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+			RenderSystem.defaultBlendFunc();
+			RenderSystem.disableBlend();
+			RenderSystem.enableCull();
+			RenderSystem.enableDepthTest();
+			RenderSystem.depthMask(true);
+		}
+	}
+
+	public static void execute(LevelAccessor world) {
+		execute(null, world);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world) {
+		if (world instanceof ClientLevel _blockEntityContext) {
+			int _scanRange = Minecraft.getInstance().options.getEffectiveRenderDistance();
+			BlockPos _scanCenter = Minecraft.getInstance().player.blockPosition();
+			LevelChunk _levelChunk;
+			BlockState blockstateiterator;
+			int positionx, positiony, positionz;
+			for (int _chunkZ = -_scanRange; _chunkZ <= _scanRange; ++_chunkZ) {
+				for (int _chunkX = -_scanRange; _chunkX <= _scanRange; ++_chunkX) {
+					_levelChunk = _blockEntityContext.getChunk(SectionPos.blockToSectionCoord(_scanCenter.getX() + (_chunkX << 4)), SectionPos.blockToSectionCoord(_scanCenter.getZ() + (_chunkZ << 4)));
+					if (_levelChunk != null) {
+						for (Map.Entry<BlockPos, BlockEntity> _blockEntityEntry : _levelChunk.getBlockEntities().entrySet()) {
+							blockstateiterator = _blockEntityEntry.getValue().getBlockState();
+							positionx = _blockEntityEntry.getKey().getX();
+							positiony = _blockEntityEntry.getKey().getY();
+							positionz = _blockEntityEntry.getKey().getZ();
+							if ((world.getBlockState(new BlockPos(positionx, positiony, positionz))).getBlock() == PowerModBlocks.ENERGIUM_VAULT.get()) {
+								if (((world.getBlockState(new BlockPos(positionx, positiony, positionz))).getBlock().getStateDefinition().getProperty("blockstate") instanceof IntegerProperty _getip9
+										? (world.getBlockState(new BlockPos(positionx, positiony, positionz))).getValue(_getip9)
+										: -1) == 1) {
+									renderItem(new ItemStack(PowerModItems.ENERGIUM_UPGRADE_SMITHING_TEMPLATE.get()), (positionx + 0.5), (positiony + 0.5), (positionz + 0.5), (float) PowerModVariables.WorldVariables.get(world).entity_rotation, -30,
+											0, (float) 0.4, false, true);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
