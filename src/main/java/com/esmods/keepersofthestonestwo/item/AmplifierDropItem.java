0a7d5c31b20d19533f9475a63921be84cfac883a
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-09-29 12:05:47 +0300

    Some fixes

--- src/main/java/com/esmods/keepersofthestonestwo/item/AmplifierDropItem.java
+++ src/main/java/com/esmods/keepersofthestonestwo/item/AmplifierDropItem.java
@@ -1,24 +1,11 @@
 
 package com.esmods.keepersofthestonestwo.item;
 
-import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.entity.LivingEntity;
 
 public class AmplifierDropItem extends Item {
 	public AmplifierDropItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
-	}
-
-	@Override
-	public UseAnim getUseAnimation(ItemStack itemstack) {
-		return UseAnim.BOW;
-	}
-
-	@Override
-	public int getUseDuration(ItemStack itemstack, LivingEntity livingEntity) {
-		return 32;
 	}
 }
