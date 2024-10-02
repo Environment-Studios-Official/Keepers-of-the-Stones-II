7f956ea13e0d0a304fc6672c2ea67e0373c3ef2b
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-10-03 00:47:30 +0300

    Improved generation for Cursed Forest

--- src/main/java/com/esmods/keepersofthestonestwo/init/PowerModBiomes.java
+++ src/main/java/com/esmods/keepersofthestonestwo/init/PowerModBiomes.java
@@ -51,16 +51,10 @@ public class PowerModBiomes {
 				// Inject biomes to biome source
 				if (chunkGenerator.getBiomeSource() instanceof MultiNoiseBiomeSource noiseSource) {
 					List<Pair<Climate.ParameterPoint, Holder<Biome>>> parameters = new ArrayList<>(noiseSource.parameters().values());
-					addParameterPoint(parameters,
-							new Pair<>(
-									new Climate.ParameterPoint(Climate.Parameter.span(-0.45f, 0.15f), Climate.Parameter.span(-0.35f, 0.3f), Climate.Parameter.span(-0.19f, 0.03f), Climate.Parameter.span(-0.375f, 0.2225f),
-											Climate.Parameter.point(0.0f), Climate.Parameter.span(-1f, -0.3555f), 0),
-									biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("power", "cursed_forest")))));
-					addParameterPoint(parameters,
-							new Pair<>(
-									new Climate.ParameterPoint(Climate.Parameter.span(-0.45f, 0.15f), Climate.Parameter.span(-0.35f, 0.3f), Climate.Parameter.span(-0.19f, 0.03f), Climate.Parameter.span(-0.375f, 0.2225f),
-											Climate.Parameter.point(1.0f), Climate.Parameter.span(-1f, -0.3555f), 0),
-									biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("power", "cursed_forest")))));
+					addParameterPoint(parameters, new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.15f, 0.2f), Climate.Parameter.span(-0.1f, 0.1f), Climate.Parameter.span(-0.19f, 0.03f), Climate.Parameter.span(-0.375f, 0.2225f),
+							Climate.Parameter.point(0.0f), Climate.Parameter.span(-1f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("power", "cursed_forest")))));
+					addParameterPoint(parameters, new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.15f, 0.2f), Climate.Parameter.span(-0.1f, 0.1f), Climate.Parameter.span(-0.19f, 0.03f), Climate.Parameter.span(-0.375f, 0.2225f),
+							Climate.Parameter.point(1.0f), Climate.Parameter.span(-1f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("power", "cursed_forest")))));
 					chunkGenerator.biomeSource = MultiNoiseBiomeSource.createFromList(new Climate.ParameterList<>(parameters));
 					chunkGenerator.featuresPerStep = Suppliers
 							.memoize(() -> FeatureSorter.buildFeaturesPerStep(List.copyOf(chunkGenerator.biomeSource.possibleBiomes()), biome -> chunkGenerator.generationSettingsGetter.apply(biome).features(), true));
