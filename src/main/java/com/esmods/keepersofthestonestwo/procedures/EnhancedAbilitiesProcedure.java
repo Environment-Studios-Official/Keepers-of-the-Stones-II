707b7decd6a1f65eebc6440cc1eb756aba50b8d9
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-10-10 23:52:26 +0300

    The power lock effect now blocks the buff effects of stones

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/EnhancedAbilitiesProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/EnhancedAbilitiesProcedure.java
@@ -5,15 +5,19 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 
+import com.esmods.keepersofthestonestwo.network.PowerModVariables;
+
 public class EnhancedAbilitiesProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 60, 2, false, false));
-		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 60, 2, false, false));
-		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.JUMP, 60, 2, false, false));
+		if (!entity.getData(PowerModVariables.PLAYER_VARIABLES).ability_block) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 60, 2, false, false));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 60, 2, false, false));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.JUMP, 60, 2, false, false));
+		}
 	}
 }
