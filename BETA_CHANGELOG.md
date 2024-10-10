ed16de0ca5af94cc0d17a8a73bf85b997d9dfcdc
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-10-10 18:59:05 +0300

    Added new gamerule - choiceStoneWhenEnterTheWorld

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,3 +1,4 @@
 What's new in Keepers of the Stones II version 1.0.0.47?<br/>
+<br />- Added new gamerule - choiceStoneWhenEnterTheWorld
 <br />- The explosion of the third lightning and explosion abilities has been slightly reduced
 <br />- [Bugfix] 
---
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/OpenChoiceKeeperBoxStoneScreenProcedure.java
@@ -0,0 +1,63 @@
+package com.esmods.keepersofthestonestwo.procedures;
+
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.core.BlockPos;
+
+import javax.annotation.Nullable;
+
+import io.netty.buffer.Unpooled;
+
+import com.esmods.keepersofthestonestwo.world.inventory.KeepersBoxGUIPart1Menu;
+import com.esmods.keepersofthestonestwo.network.PowerModVariables;
+import com.esmods.keepersofthestonestwo.init.PowerModGameRules;
+
+@EventBusSubscriber
+public class OpenChoiceKeeperBoxStoneScreenProcedure {
+	@SubscribeEvent
+	public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+		execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity());
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (world.getLevelData().getGameRules().getBoolean(PowerModGameRules.CHOICE_STONE_WHEN_ENTER_THE_WORLD) == true && entity.getData(PowerModVariables.PLAYER_VARIABLES).selected == false) {
+			if (entity instanceof ServerPlayer _ent) {
+				BlockPos _bpos = BlockPos.containing(x, y, z);
+				_ent.openMenu(new MenuProvider() {
+					@Override
+					public Component getDisplayName() {
+						return Component.literal("KeepersBoxGUIPart1");
+					}
+
+					@Override
+					public boolean shouldTriggerClientSideContainerClosingOnOpen() {
+						return false;
+					}
+
+					@Override
+					public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+						return new KeepersBoxGUIPart1Menu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(_bpos));
+					}
+				}, _bpos);
+			}
+		}
+	}
+}
