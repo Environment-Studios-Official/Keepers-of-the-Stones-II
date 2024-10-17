195ef3821cc92e6d5f522ba36625b03d6b530115
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-10-18 00:12:24 +0300

    More fixes

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -4,5 +4,8 @@ What's new in Keepers of the Stones II version 1.0.0.47?<br/>
 <br />- Teleportation stone abilities have been reworked
 <br />- The power lock effect now blocks the buff effects of stones
 <br />- The explosion of the third lightning and explosion abilities has been slightly reduced
+<br />- Added missing crafting recipes
+<br />- [Bugfix] Fixed a bug where the core of an energium golem could break unbreakble blocks
+<br />- [Bugfix] Fixed a bug that could cause bosses and knights to get into boats and minecarts
 <br />- [Bugfix] Fixed a bug that caused the abilities of the time stone to be mixed up
 <br />- [Bugfix] Fixed a bug that could cause a atomic bomb to be used for grifering, even if a defense was installed
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/CursedKeeperPriObnovlieniiTikaSushchnostiProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/CursedKeeperPriObnovlieniiTikaSushchnostiProcedure.java
@@ -1,10 +1,18 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraft.world.entity.vehicle.ChestBoat;
+import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.commands.arguments.EntityAnchorArgument;
+
+import java.util.Comparator;
 
 public class CursedKeeperPriObnovlieniiTikaSushchnostiProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
@@ -52,6 +60,25 @@ public class CursedKeeperPriObnovlieniiTikaSushchnostiProcedure {
 			entity.getPersistentData().putDouble("IA", 0);
 			entity.getPersistentData().putDouble("Patience", 0);
 			entity.getPersistentData().putBoolean("OnBattle", false);
+		}
+		if (!world.getEntitiesOfClass(Boat.class, AABB.ofSize(new Vec3(x, y, z), 1, 1, 1), e -> true).isEmpty()) {
+			((Entity) world.getEntitiesOfClass(Boat.class, AABB.ofSize(new Vec3(x, y, z), 1, 1, 1), e -> true).stream().sorted(new Object() {
+				Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+					return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+				}
+			}.compareDistOf(x, y, z)).findFirst().orElse(null)).hurt(new DamageSource(world.holderOrThrow(DamageTypes.GENERIC)), 1000);
+		} else if (!world.getEntitiesOfClass(ChestBoat.class, AABB.ofSize(new Vec3(x, y, z), 1, 1, 1), e -> true).isEmpty()) {
+			((Entity) world.getEntitiesOfClass(ChestBoat.class, AABB.ofSize(new Vec3(x, y, z), 1, 1, 1), e -> true).stream().sorted(new Object() {
+				Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+					return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+				}
+			}.compareDistOf(x, y, z)).findFirst().orElse(null)).hurt(new DamageSource(world.holderOrThrow(DamageTypes.GENERIC)), 1000);
+		} else if (!world.getEntitiesOfClass(Minecart.class, AABB.ofSize(new Vec3(x, y, z), 1, 1, 1), e -> true).isEmpty()) {
+			((Entity) world.getEntitiesOfClass(Minecart.class, AABB.ofSize(new Vec3(x, y, z), 1, 1, 1), e -> true).stream().sorted(new Object() {
+				Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+					return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+				}
+			}.compareDistOf(x, y, z)).findFirst().orElse(null)).hurt(new DamageSource(world.holderOrThrow(DamageTypes.GENERIC)), 1000);
 		}
 	}
 }
